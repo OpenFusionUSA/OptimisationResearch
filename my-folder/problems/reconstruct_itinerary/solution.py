06e3fb7f89dd2508b1da39222ccccc2fae46f9b5
OpenFusionUSA <web.developer@openfusion.us>
2023-11-04 18:37:37 +0000

    Sync LeetCode submission - Reconstruct Itinerary (python3)

--- my-folder/problems/reconstruct_itinerary/solution.py
+++ my-folder/problems/reconstruct_itinerary/solution.py
@@ -1,23 +1,16 @@
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
-        adj={src:[] for src,dst in tickets}
-        tickets.sort()
-        for src,dest in tickets:
-            adj[src].append(dest)
-        res = ["JFK"] 
-        def dfs(src):
-            
-            if(len(res)==len(tickets)+1): return True
-           
-            if(src not in adj): return False
-            temp=list(adj[src])
-            
-            for i,v in enumerate(temp):
-                adj[src].pop(i)
-                res.append(v)
-                if dfs(v): return True
-                adj[src].insert(i,v)
-                res.pop()
-        dfs("JFK")
-        return res
+        graph = defaultdict(list)
         
+        for src, dst in sorted(tickets, reverse=True):
+            graph[src].append(dst)
+            
+        itinerary = []
+        def dfs(airport):
+            while graph[airport]:
+                dfs(graph[airport].pop())
+            itinerary.append(airport)
+        
+        dfs("JFK")
+        
+        return itinerary[::-1]
