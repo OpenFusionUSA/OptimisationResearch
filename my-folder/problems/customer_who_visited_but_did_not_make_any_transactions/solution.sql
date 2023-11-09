fde0ff315da1e96c65d09e12aef84a1080fa9770
OpenFusionUSA <web.developer@openfusion.us>
2023-11-09 17:28:35 +0000

    Sync LeetCode submission - Customer Who Visited but Did Not Make Any Transactions (mysql)

--- my-folder/problems/customer_who_visited_but_did_not_make_any_transactions/solution.sql
+++ my-folder/problems/customer_who_visited_but_did_not_make_any_transactions/solution.sql
@@ -1,6 +1,6 @@
-# Write your MySQL query statement below
-
-
-#select distinct(visit_id) from Transactions;
-
-select distinct(customer_id), count(*) as count_no_trans from Visits where visit_id not in (select distinct(visit_id) from Transactions) group by customer_id order by count_no_trans ASC;
+SELECT v.customer_id, COUNT(v.visit_id) AS count_no_trans
+FROM Visits v
+LEFT JOIN Transactions t ON v.visit_id = t.visit_id
+WHERE t.visit_id IS NULL
+GROUP BY v.customer_id
+ORDER BY count_no_trans DESC;
