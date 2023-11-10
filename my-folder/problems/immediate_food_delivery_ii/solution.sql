d74d0076a6465f5817aa5a7858368ac71fe72d79
OpenFusionUSA <web.developer@openfusion.us>
2023-11-10 23:31:41 +0000

    Sync LeetCode submission - Immediate Food Delivery II (mysql)

--- my-folder/problems/immediate_food_delivery_ii/solution.sql
+++ my-folder/problems/immediate_food_delivery_ii/solution.sql
@@ -7,7 +7,11 @@
 #select customer_id,SUM(if(Min(order_date)= customer_pref_delivery_date,1,0)) as a,SUM(1) as b from Delivery group by customer_id;
 
 #select delivery_id,Min(order_date) as a,SUM(1) as b from Delivery group by customer_id;
+select round((sum(case when order_date = customer_pref_delivery_date then 1 else 0 end)/ sum(1) *100) ,2) as immediate_percentage
+from delivery
+where (customer_id, order_date) in (select customer_id, min(order_date) as order_date 
+from Delivery
+group by customer_id);
 
 
-select ROUND(SUM(if(order_date=customer_pref_delivery_date,1,0))/SUM(1)*100,2)as immediate_percentage  from Delivery where (customer_id,order_date) in ( select customer_id,Min(order_date) as order_date from Delivery group by customer_id );
 
