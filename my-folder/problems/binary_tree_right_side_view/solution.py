009b1aeccf52ed1bdb5802d81d278eb3c8904a81
OpenFusionUSA <web.developer@openfusion.us>
2023-11-11 23:47:23 +0000

    Sync LeetCode submission - Binary Tree Right Side View (python)

--- my-folder/problems/binary_tree_right_side_view/solution.py
+++ my-folder/problems/binary_tree_right_side_view/solution.py
@@ -1,26 +1,26 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        queue = []
-        queue.append(root)
-        right_view = []
-        if root == None:
-            return []
-        while len(queue) != 0:
-            back = queue[- 1]
-            right_view.append(back.val)
-            level_queue = []
-            while queue != []:
-                front = queue.pop(0)
-                if front.left != None:
-                    level_queue.append(front.left)
-                if front.right != None:
-                    level_queue.append(front.right)
-            queue = level_queue
-        # print(right_view)
-        return right_view
+class Solution(object):
+    def rightSideView(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if root is None:
+            return root
+        ans=[]
+        q=collections.deque()
+        q.append(root)
+        while q:
+            ans.append(q[-1].val)
+            for i in range(len(q)):
+                val=q.popleft()
+                if val.left is not None:
+                    q.append(val.left)
+                if val.right is not None:
+                    q.append(val.right)
+        return ans
