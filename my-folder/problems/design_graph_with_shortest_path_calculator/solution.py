b82081aaa39b987c09aaef1be17ff4ea3325e995
OpenFusionUSA <web.developer@openfusion.us>
2023-11-11 21:42:10 +0000

    Sync LeetCode submission - Design Graph With Shortest Path Calculator (python)

--- my-folder/problems/design_graph_with_shortest_path_calculator/solution.py
+++ my-folder/problems/design_graph_with_shortest_path_calculator/solution.py
@@ -1,29 +1,47 @@
-class Graph:
+class Graph(object):
 
-    def __init__(self, n: int, edges: List[List[int]]):
-        self.adj_list = [[] for _ in range(n)]
-        for from_node, to_node, cost in edges:
-            self.adj_list[from_node].append((to_node, cost))
+    def __init__(self, n, edges):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        """
+        self.adj_list=[[] for _ in range(n)]
+        for i,j,k in edges:
+            self.adj_list[i].append((j,k))
 
-    def addEdge(self, edge: List[int]) -> None:
-        from_node, to_node, cost = edge
-        self.adj_list[from_node].append((to_node, cost))
+    def addEdge(self, edge):
+        """
+        :type edge: List[int]
+        :rtype: None
+        """
+        i,j,k=edge
+        self.adj_list[i].append((j,k))
+        
 
-    def shortestPath(self, node1: int, node2: int) -> int:
-        n = len(self.adj_list)
-        pq = [(0, node1)]
-        cost_for_node = [inf] * (n)
-        cost_for_node[node1] = 0
-
+    def shortestPath(self, node1, node2):
+        """
+        :type node1: int
+        :type node2: int
+        :rtype: int
+        """
+        n=len(self.adj_list)
+        pq=[]
+        heapq.heappush(pq,(0,node1))
+        distance=[float('inf')] * n
+        distance[node1]=0
         while pq:
-            curr_cost, curr_node = heappop(pq)
-            if curr_cost > cost_for_node[curr_node]:
+            cost,node=heappop(pq)
+            if cost > distance[node]:
                 continue
-            if curr_node == node2:
-                return curr_cost
-            for neighbor, cost in self.adj_list[curr_node]:
-                new_cost = curr_cost + cost
-                if new_cost < cost_for_node[neighbor]:
-                    cost_for_node[neighbor] = new_cost
-                    heappush(pq, (new_cost, neighbor))
+            if node == node2:
+                return cost
+            for n,c in self.adj_list[node]:
+                nc=cost+c
+                if nc<distance[n]:
+                    distance[n]=nc
+                    heappush(pq,(nc,n))
         return -1
+# Your Graph object will be instantiated and called as such:
+# obj = Graph(n, edges)
+# obj.addEdge(edge)
+# param_2 = obj.shortestPath(node1,node2)
