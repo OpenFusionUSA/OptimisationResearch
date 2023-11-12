3e38a2f4a843bdd026597db688cd81007fc28e59
OpenFusionUSA <web.developer@openfusion.us>
2023-11-12 18:39:24 +0000

    Sync LeetCode submission - Customers Who Bought All Products (mysql)

---
+++ my-folder/problems/customers_who_bought_all_products/solution.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select customer_id
+from Customer
+group by customer_id
+HAVING COUNT(DISTINCT product_key) = 
+    (SELECT COUNT(DISTINCT product_key) FROM Product);
