92e131b66582714ebe287a96b5084a13f3cbb6af
OpenFusionUSA <web.developer@openfusion.us>
2023-11-13 03:02:07 +0000

    Sync LeetCode submission - Validate Binary Search Tree (python)

---
+++ my-folder/problems/validate_binary_search_tree/solution.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+import math
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        def validate(node,low=float('-inf'),high=float('inf')):
+            if not node:
+                return True
+            if node.val<=low or node.val>=high:
+                return False
+            return (validate(node.left,low,node.val) and validate(node.right,node.val,high))
+        return validate(root)
