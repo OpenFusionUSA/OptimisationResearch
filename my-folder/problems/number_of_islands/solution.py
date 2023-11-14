30e8b6683b7ec594a59a4b6b712e9d00d9ec978c
OpenFusionUSA <web.developer@openfusion.us>
2023-11-14 23:17:20 +0000

    Sync LeetCode submission - Number of Islands (python)

--- my-folder/problems/number_of_islands/solution.py
+++ my-folder/problems/number_of_islands/solution.py
@@ -6,22 +6,21 @@ class Solution(object):
         """
         m=len(grid)
         n=len(grid[0])
-        dir = ((1,0),(0,1),(-1,0),(0,-1))
         island=0
+        dir=((1,0),(0,1),(-1,0),(0,-1))
         for i in range(m):
             for j in range(n):
                 if grid[i][j]=="1":
                     island+=1
-                    # write bfs code here 
-                    grid[i][j]="0"
-                    q=deque()
+                    q=collections.deque()
                     q.append((i,j))
                     while q:
-                        a,b=q.popleft()
-                        for k,l in (dir):
-                            x,y=a+k,b+l
-                            if 0<=x<m and 0<=y<n and grid[x][y]=="1":
-                                grid[x][y]="0"
-                                q.append((x,y))
+                        k,l=q.popleft()
+                        for p,r in dir:
+                            modi=k+p
+                            modj=l+r
+                            if modi>=0 and modj>=0 and modi<m and modj<n and grid[modi][modj]=="1":
+                                grid[modi][modj]="0"
+                                q.append((modi,modj))
         return island
-                    
+                            
