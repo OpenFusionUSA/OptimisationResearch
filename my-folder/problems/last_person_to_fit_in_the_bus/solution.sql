4e9e5ccacc39fefd648c73b1593f0fdb1c1005b8
OpenFusionUSA <web.developer@openfusion.us>
2023-11-14 00:30:19 +0000

    Sync LeetCode submission - Last Person to Fit in the Bus (mysql)

--- my-folder/problems/last_person_to_fit_in_the_bus/solution.sql
+++ my-folder/problems/last_person_to_fit_in_the_bus/solution.sql
@@ -1,12 +1,7 @@
-SELECT 
-    person_name
-FROM (
-    SELECT
-        person_name,
-        weight,
-        SUM(weight) OVER(ORDER BY turn) as total_weight
-    FROM Queue
-    ) x
-WHERE total_weight <= 1000
-ORDER BY total_weight DESC
-LIMIT 1
+select a1.person_name
+from Queue a1
+join Queue a2 on a1.turn >= a2.turn
+group by a1.turn
+having sum(a2.weight) <= 1000
+order by sum((a2.weight)) desc
+limit 1
