c71649ceac031922610b91da8e8e8c8b3540c26b
OpenFusionUSA <web.developer@openfusion.us>
2023-11-16 22:23:30 +0000

    Sync LeetCode submission - Find Unique Binary String (python)

---
+++ my-folder/problems/find_unique_binary_string/solution.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def findDifferentBinaryString(self, nums):
+        """
+        :type nums: List[str]
+        :rtype: str
+        """
+        integers=set()
+        for num in nums:
+            integers.add(int(num,2))
+        n = len(nums)
+        for num in range(n+1):
+            if num not in integers:
+                ans=bin(num)[2:]
+                return "0"*(n-len(ans))+ans
+        return ""
