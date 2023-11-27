1acd06466c869dcf689de82ab748ad284b337dca
OpenFusionUSA <web.developer@openfusion.us>
2023-11-27 06:59:51 +0000

    Sync LeetCode submission - Largest Submatrix With Rearrangements (python)

--- my-folder/problems/largest_submatrix_with_rearrangements/solution.py
+++ my-folder/problems/largest_submatrix_with_rearrangements/solution.py
@@ -1,16 +1,17 @@
-class Solution:
-    def largestSubmatrix(self, matrix: List[List[int]]) -> int:
-        m = len(matrix)
-        n = len(matrix[0])
-        ans = 0
-
-        for row in range(m):
-            for col in range(n):
-                if matrix[row][col] != 0 and row > 0:
-                    matrix[row][col] += matrix[row - 1][col]
-
-            curr_row = sorted(matrix[row], reverse=True)
+class Solution(object):
+    def largestSubmatrix(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: int
+        """
+        ans=0
+        m=len(matrix)
+        n=len(matrix[0])
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j]!=0 and i>0:
+                    matrix[i][j]=matrix[i][j]+matrix[i-1][j]
+            curr_row = sorted(matrix[i], reverse=True)
             for i in range(n):
-                ans = max(ans, curr_row[i] * (i + 1))
-
+                    ans = max(ans, curr_row[i] * (i + 1))
         return ans
