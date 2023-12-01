cae4c4fc48f368a557873e3a6b6b8a48d57dab2c
OpenFusionUSA <web.developer@openfusion.us>
2023-12-01 03:23:05 +0000

    Sync LeetCode submission - Product of Array Except Self (python)

--- my-folder/problems/product_of_array_except_self/solution.py
+++ my-folder/problems/product_of_array_except_self/solution.py
@@ -7,9 +7,8 @@ class Solution(object):
         out=[1]*len(nums)
         for i in range(1,len(nums)):
             out[i]=out[i-1]*nums[i-1]
-        print(out)
         r=1
         for i in range(len(nums)-2,-1,-1):
             r*=nums[i+1]
-            out[i]=out[i]*r 
+            out[i]=out[i]*r
         return out
