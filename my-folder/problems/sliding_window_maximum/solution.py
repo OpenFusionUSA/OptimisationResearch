dbc3117824f0a75c68d979d395f560333da47aee
OpenFusionUSA <web.developer@openfusion.us>
2023-12-12 16:45:38 +0000

    Sync LeetCode submission - Sliding Window Maximum (python)

---
+++ my-folder/problems/sliding_window_maximum/solution.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def maxSlidingWindow(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        q=deque()
+        res=[]
+        for i in range(k):
+            while q and nums[i]>nums[q[-1]]:
+                q.pop()
+            q.append(i)
+        res.append(nums[q[0]])
+        for i in range(k,len(nums)):
+            if q and q[0]==i-k:
+                q.popleft()
+            while q and nums[i]>nums[q[-1]]:
+                q.pop()
+            q.append(i)
+            res.append(nums[q[0]])
+        return res
