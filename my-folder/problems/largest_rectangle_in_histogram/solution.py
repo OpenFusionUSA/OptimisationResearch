f237a09ee07e1645e4933051cf2228be9464875b
OpenFusionUSA <web.developer@openfusion.us>
2023-12-13 20:39:03 +0000

    Sync LeetCode submission - Largest Rectangle in Histogram (python3)

---
+++ my-folder/problems/largest_rectangle_in_histogram/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        marea=0
+        stack=[]
+        n=len(heights)
+        for i,h in enumerate(heights):
+            startIndex=i
+            while stack and h<stack[-1][1]:
+                startIndex,height=stack.pop()
+                marea=max(height*(i-startIndex),marea)
+            stack.append([startIndex,h])
+        while stack:
+            startIndex,height=stack.pop()
+            marea=max(height*(n-startIndex),marea)
+        return marea
