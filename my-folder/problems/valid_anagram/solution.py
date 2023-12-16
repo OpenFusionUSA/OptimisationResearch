c9a68ea30b415ee10c01e07f33c12d9d531d13c7
OpenFusionUSA <web.developer@openfusion.us>
2023-12-16 02:46:01 +0000

    Sync LeetCode submission - Valid Anagram (python3)

--- my-folder/problems/valid_anagram/solution.py
+++ my-folder/problems/valid_anagram/solution.py
@@ -1,14 +1,3 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        sd={}
-        td={}
-        for char in s:
-            sd[char]=sd.get(char,0)+1
-        for char in t:
-            td[char]=td.get(char,0)+1
-        return sd==td
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return collections.Counter(s)==collections.Counter(t)
