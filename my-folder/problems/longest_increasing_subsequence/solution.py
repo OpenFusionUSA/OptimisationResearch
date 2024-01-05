97797d82621718aa7489a0093c16709b9b4b28d8
OpenFusionUSA <web.developer@openfusion.us>
2024-01-05 19:34:42 +0000

    Sync LeetCode submission - Longest Increasing Subsequence (python3)

--- my-folder/problems/longest_increasing_subsequence/solution.py
+++ my-folder/problems/longest_increasing_subsequence/solution.py
@@ -1,13 +1,10 @@
-class Solution(object):
-    def lengthOfLIS(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        dp=[1]*len(nums)
-        for i in range(1,len(nums)):
-            for j in range(i):
-                if nums[i] > nums[j]:
-                    dp[i]=max(dp[i],dp[j]+1)
-        return max(dp)
-        
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        sub=[]
+        for num in nums:
+            i=bisect_left(sub,num)
+            if i==len(sub):
+                sub.append(num)
+            else:
+                sub[i]=num
+        return len(sub)
