5f9b9c859d1c3455ffdc70014a9a1cb229e0dd3d
OpenFusionUSA <web.developer@openfusion.us>
2024-01-09 18:25:11 +0000

    Sync LeetCode submission - Arithmetic Slices II - Subsequence (python3)

--- my-folder/problems/arithmetic_slices_ii_-_subsequence/solution.py
+++ my-folder/problems/arithmetic_slices_ii_-_subsequence/solution.py
@@ -1,11 +1,10 @@
 class Solution:
     def numberOfArithmeticSlices(self, nums: List[int]) -> int:
         n=len(nums)
-        dp=[defaultdict(int) for _ in range(n)]
         ans=0
+        dp=[defaultdict(int) for _ in range(n)]
         for i in range(1,n):
             for j in range(i):
-                diff=nums[i]-nums[j]
-                ans+=dp[j][diff]
-                dp[i][diff]+=dp[j][diff]+1
+                ans+=dp[j][nums[i]-nums[j]]
+                dp[i][nums[i]-nums[j]]+=dp[j][nums[i]-nums[j]]+1
         return ans
