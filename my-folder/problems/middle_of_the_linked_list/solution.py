2cbbeb92f62aa726dbda1d8656d5cd8286ad107a
OpenFusionUSA <web.developer@openfusion.us>
2024-01-16 23:44:15 +0000

    Sync LeetCode submission - Middle of the Linked List (python3)

--- my-folder/problems/middle_of_the_linked_list/solution.py
+++ my-folder/problems/middle_of_the_linked_list/solution.py
@@ -1,17 +1,13 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def middleNode(self, head):
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """
-        slow=fast=head
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow=head
+        fast=head
         while fast and fast.next:
-            slow = slow.next
+            slow=slow.next
             fast=fast.next.next
         return slow
-        
