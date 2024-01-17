822fe7529ac4c20208ad1adb88ac4f5723c5e9b3
OpenFusionUSA <web.developer@openfusion.us>
2024-01-17 06:32:22 +0000

    Sync LeetCode submission - Reverse Linked List II (python3)

--- my-folder/problems/reverse_linked_list_ii/solution.py
+++ my-folder/problems/reverse_linked_list_ii/solution.py
@@ -1,25 +1,27 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
-
         if not head or left == right:
             return head
 
+        # Create a dummy node to simplify edge cases
         dummy = ListNode(0, head)
         prev = dummy
 
+        # Move prev to the node just before the left position
         for _ in range(left - 1):
             prev = prev.next
 
+        # Start the reversal process
+        reverse = None
         cur = prev.next
-        for _ in range(right - left):
-            temp = cur.next
-            cur.next = temp.next
-            temp.next = prev.next
-            prev.next = temp
+        for _ in range(right - left + 1):
+            nxt = cur.next
+            cur.next = reverse
+            reverse = cur
+            cur = nxt
+
+        # Connect the reversed sublist back to the main list
+        prev.next.next = cur
+        prev.next = reverse
 
         return dummy.next
