b326c1ace53943e209859f8d3fdfba77296ea4a0
OpenFusionUSA <web.developer@openfusion.us>
2024-01-21 18:47:12 +0000

    Sync LeetCode submission - House Robber (python3)

--- my-folder/problems/house_robber/solution.py
+++ my-folder/problems/house_robber/solution.py
@@ -1,18 +1,11 @@
-class Solution(object):
-    def rob(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        if len(nums)==0:
-            return 
-        if len(nums)==1:
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n=len(nums)
+        if n==1:
             return nums[0]
-        if len(nums)==2:
-            return max(nums[0],nums[1])
-        amount=[0]*(len(nums))
-        amount[0]=nums[0]
-        amount[1]=max(amount[0],nums[1])
-        for i in range(2,len(nums)):
-            amount[i]=max(amount[i-1],amount[i-2]+nums[i])
-        return amount[-1]
+        dp=[0]*n
+        dp[0]=nums[0]
+        dp[1]=max(nums[0],nums[1])
+        for i in range(2,n):
+            dp[i]=max(dp[i-1],dp[i-2]+nums[i])
+        return dp[n-1]
