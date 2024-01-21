6687f696d7ba3e6bfd62793e18c7b34ca22dc0fe
OpenFusionUSA <web.developer@openfusion.us>
2024-01-21 23:01:09 +0000

    Sync LeetCode submission - Binary Tree Maximum Path Sum (python3)

--- my-folder/problems/binary_tree_maximum_path_sum/solution.py
+++ my-folder/problems/binary_tree_maximum_path_sum/solution.py
@@ -1,25 +1,18 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    
-    def maxPathSum(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
-        res = [root.val]
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        s=[root.val]
         def dfs(node):
             if not node:
                 return 0
-            leftMax=max(dfs(node.left),0)
-            rightMax=max(dfs(node.right),0)
-            res[0]=max(res[0],node.val+leftMax+rightMax)
-            return node.val+max(leftMax,rightMax)
-        
+            leftsum=max(dfs(node.left),0)
+            rightsum=max(dfs(node.right),0)
+            s[0]=max(s[0],node.val+leftsum+rightsum)
+            return node.val+max(leftsum,rightsum)
         dfs(root)
-        return res[0]
-        
+        return s [0] 
