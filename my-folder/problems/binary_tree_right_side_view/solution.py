dd75dd37f86f02dd7c6512912d065d9eeed83902
OpenFusionUSA <web.developer@openfusion.us>
2024-01-22 21:30:45 +0000

    Sync LeetCode submission - Binary Tree Right Side View (python3)

--- my-folder/problems/binary_tree_right_side_view/solution.py
+++ my-folder/problems/binary_tree_right_side_view/solution.py
@@ -1,26 +1,25 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def rightSideView(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[int]
-        """
-        if root is None:
-            return root
-        ans=[]
-        q=collections.deque()
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        q=[]
         q.append(root)
-        while q:
-            ans.append(q[-1].val)
+        r=[]
+        if root is None:
+            return None
+        while q!=[]:
+            r.append(q[-1].val)
+            nq=[]
             for i in range(len(q)):
-                val=q.popleft()
-                if val.left is not None:
-                    q.append(val.left)
-                if val.right is not None:
-                    q.append(val.right)
-        return ans
+                node=q[i]
+                if node.left:
+                    nq.append(node.left)
+                if node.right:
+                    nq.append(node.right)
+            q=nq
+        return r
+
