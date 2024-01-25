f0059c1a648e0dc470935ae891e82a811698f21a
OpenFusionUSA <web.developer@openfusion.us>
2024-01-25 19:24:42 +0000

    Sync LeetCode submission - Valid Parentheses (python3)

--- my-folder/problems/valid_parentheses/solution.py
+++ my-folder/problems/valid_parentheses/solution.py
@@ -1,16 +1,10 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        stack=[]
-        m={")":"(","}":"{","]":"["}
+class Solution:
+    def isValid(self, s: str) -> bool:
+        q=[]
+        d={"}":"{",")":"(","]":"["}
         for c in s:
-            if c in m:
-                top=stack.pop() if stack else '#'
-                if top!=m[c]:
-                    return False
-            else:
-                stack.append(c)
-        return not stack
+            if c not in d:
+                q.append(c)
+            elif q==[] or d[c]!=q.pop():
+                return False       
+        return q==[]
