42d38327808829b2e7d013561b8124ac5f78656a
OpenFusionUSA <web.developer@openfusion.us>
2024-01-25 21:40:55 +0000

    Sync LeetCode submission - Evaluate Reverse Polish Notation (python3)

--- my-folder/problems/evaluate_reverse_polish_notation/solution.py
+++ my-folder/problems/evaluate_reverse_polish_notation/solution.py
@@ -1,27 +1,19 @@
-class Solution(object):
-    def evalRPN(self, tokens):
-        """
-        :type tokens: List[str]
-        :rtype: int
-        """
-        stack=[]
-        eq=["+","-","*","/"]
-        for c in tokens:
-            if c not in eq:
-                stack.append(c)
-            else:
-                if c=="+":
-                    s=int(stack.pop())+int(stack.pop())
-                    stack.append(s)
-                elif c=="-":
-                    a=int(stack.pop())
-                    b=int(stack.pop())
-                    stack.append(b-a)
-                elif c=="*":
-                    stack.append(int(stack.pop())*int(stack.pop()))
-                elif c=="/":
-                    a=int(stack.pop())
-                    b=int(stack.pop())
-                    stack.append(int(float(b)/a))
-        return int(stack.pop())
-        
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        s=[]
+        for o in tokens:
+            v=0
+            if o not in ["+","-","*","/"]:
+                v=int(o)
+            elif o=="+":
+                v=s.pop()+s.pop()
+            elif o=="-":
+                dum=s.pop()
+                v=s.pop()-dum
+            elif o=="*":
+                v=s.pop()*s.pop()
+            elif o=="/":
+                div=s.pop()
+                v=int(s.pop()/div)
+            s.append(v)
+        return s.pop()
