31e37ef62d05cf5412fd11dc174e81985eece324
OpenFusionUSA <web.developer@openfusion.us>
2024-01-26 01:53:52 +0000

    Sync LeetCode submission - Climbing Stairs (python3)

--- my-folder/problems/climbing_stairs/solution.py
+++ my-folder/problems/climbing_stairs/solution.py
@@ -1,10 +1,13 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n==1:
-            return 1
-        dp=[0]*(n+1)
-        dp[1]=1
-        dp[2]=2
-        for i in range(3,n+1):
-            dp[i]=dp[i-1]+dp[i-2]
-        return dp[n]
+        d={1:1,2:2}
+        def recur(n):
+            if n in d:
+                return d[n]
+            if n==1:
+                return 1
+            elif n==2:
+                return 2
+            d[n]=recur(n-1)+recur(n-2)
+            return d[n]
+        return recur(n)
