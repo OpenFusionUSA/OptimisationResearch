53d302eb20718c622f8e1093065eab79a8e26e0b
OpenFusionUSA <web.developer@openfusion.us>
2024-01-31 00:52:12 +0000

    Sync LeetCode submission - Daily Temperatures (python3)

--- my-folder/problems/daily_temperatures/solution.py
+++ my-folder/problems/daily_temperatures/solution.py
@@ -1,14 +1,15 @@
-class Solution(object):
-    def dailyTemperatures(self, temperatures):
-        """
-        :type temperatures: List[int]
-        :rtype: List[int]
-        """
-        st=[]
-        output=[0]*len(temperatures)
-        for i,t in enumerate(temperatures):
-            while st and t>st[-1][0]:
-                stackT,stackIndex=st.pop()
-                output[stackIndex]=i-stackIndex
-            st.append([t,i])
-        return output
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        answer = [0] * n
+        stack = []
+        
+        for curr_day, curr_temp in enumerate(temperatures):
+            # Pop until the current day's temperature is not
+            # warmer than the temperature at the top of the stack
+            while stack and temperatures[stack[-1]] < curr_temp:
+                prev_day = stack.pop()
+                answer[prev_day] = curr_day - prev_day
+            stack.append(curr_day)
+        
+        return answer
