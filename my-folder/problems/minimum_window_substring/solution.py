2daf1dd11db2bd51d721a5805404e665d03f135b
OpenFusionUSA <web.developer@openfusion.us>
2024-02-04 00:52:44 +0000

    Sync LeetCode submission - Minimum Window Substring (python3)

--- my-folder/problems/minimum_window_substring/solution.py
+++ my-folder/problems/minimum_window_substring/solution.py
@@ -1,31 +1,42 @@
-class Solution(object):
-    def minWindow(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: str
-        """
-        tmap={}
-        for c in t:
-            tmap[c]=tmap.get(c,0)+1
-        uniq=len(tmap)
-        found=0
-        l=r=0
-        ans=float("inf"),None,None
-        windowcount={}
-        while r<len(s):
-            char=s[r]
-            windowcount[char]=windowcount.get(char,0)+1
-            if char in tmap and windowcount[char]==tmap[char]:
-                found+=1
-            while l<=r and found==uniq:
-                char=s[l]
-                if r-l+1<ans[0]:
-                    ans=(r-l+1,l,r)
-                windowcount[char]-=1
-                if char in tmap and windowcount[char]<tmap[char]:
-                    found-=1
-                l+=1
-            r+=1
-        return "" if ans[0]==float("inf") else s[ans[1]:ans[2]+1]
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if not t or len(t) > len(s):
+            return ""
         
+        # Dictionary to count characters in t
+        countT = {}
+        for c in t:
+            countT[c] = countT.get(c, 0) + 1
+        
+        # Initialize window pointers and the count for characters in the current window
+        l, r = 0, 0
+        count = {}
+        minLength = float('inf')
+        resp = ""
+        
+        # Variable to keep track of how many characters from t are currently matched in the window
+        have, need = 0, len(countT)
+        
+        while r < len(s):
+            # Add the current character to the window
+            count[s[r]] = count.get(s[r], 0) + 1
+            if s[r] in countT and count[s[r]] == countT[s[r]]:
+                have += 1
+            
+            # Shrink the window from the left if it already contains all needed characters
+            while have == need:
+                # Update the response if the current window is smaller
+                if (r - l + 1) < minLength:
+                    minLength = r - l + 1
+                    resp = s[l:r+1]
+                
+                # Remove the leftmost character from the window
+                count[s[l]] -= 1
+                if s[l] in countT and count[s[l]] < countT[s[l]]:
+                    have -= 1
+                l += 1
+            
+            # Expand the window by moving right
+            r += 1
+        
+        return resp
