076071c5939f9a129100ec0f04fca247a2bfc424
OpenFusionUSA <web.developer@openfusion.us>
2024-02-06 21:18:53 +0000

    Sync LeetCode submission - Group Anagrams (python3)

--- my-folder/problems/group_anagrams/solution.py
+++ my-folder/problems/group_anagrams/solution.py
@@ -1,14 +1,6 @@
 class Solution(object):
     def groupAnagrams(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
-        diclist={}
-        for i in strs:
-            key=tuple(sorted(i))
-            if key not in diclist:
-                diclist[key]=[]
-            diclist[key].append(i)
-        return [diclist[i] for i in diclist]
-        
+        ans = collections.defaultdict(list)
+        for s in strs:
+            ans[tuple(sorted(s))].append(s)
+        return ans.values()
