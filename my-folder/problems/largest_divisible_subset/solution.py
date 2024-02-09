83a311d048cb619fc1159903d26aa14a32ccc820
OpenFusionUSA <web.developer@openfusion.us>
2024-02-09 01:57:40 +0000

    Sync LeetCode submission - Largest Divisible Subset (python3)

---
+++ my-folder/problems/largest_divisible_subset/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+        subsets={-1:set()}
+        for num in sorted(nums):
+            subsets[num]=max([subsets[k] for k in subsets if num%k==0],key=len)| {num}
+        print(subsets)
+        return list(max(subsets.values(),key=len))
