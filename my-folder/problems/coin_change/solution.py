25d8164d2b471031498cf824d5b962d37bdd9b64
OpenFusionUSA <web.developer@openfusion.us>
2024-02-12 03:41:25 +0000

    Sync LeetCode submission - Coin Change (python3)

--- my-folder/problems/coin_change/solution.py
+++ my-folder/problems/coin_change/solution.py
@@ -1,11 +1,12 @@
-
-
-class Solution(object):
-    
-    def coinChange(self, coins, amount):
-        dp=[float('inf')]*(amount+1)
-        dp[0]=0
-        for coin in coins:
-            for x in range(coin,amount+1):
-                dp[x]=min(dp[x],dp[x-coin]+1)
-        return dp[amount] if dp[amount]!=float('inf') else -1
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        coins.sort()
+        @cache
+        def rec(amount):
+            if amount==0:
+                return 0
+            if amount<0:
+                return float('inf')
+            return min((rec(amount-coin) for coin in coins if coin<=amount),default=float('inf'))+1
+        res=rec(amount)
+        return res if res!=float('inf') else -1
