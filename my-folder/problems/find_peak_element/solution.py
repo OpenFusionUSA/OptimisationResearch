57dcc5a27ec602bdadfc87223894d8b4f0156a67
OpenFusionUSA <web.developer@openfusion.us>
2024-02-12 06:49:06 +0000

    Sync LeetCode submission - Find Peak Element (python3)

--- my-folder/problems/find_peak_element/solution.py
+++ my-folder/problems/find_peak_element/solution.py
@@ -1,19 +1,25 @@
+from typing import List
+
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return 0
+        n = len(nums)
+        if n == 1:
+            return 0  # The only element is the peak
         if nums[0] > nums[1]:
-            return 0
-        if nums[len(nums)-1] > nums[len(nums)-2]:
-            return len(nums)-1
-        i=1
-        j=len(nums)-2
-        while i<=j:
-            mid=i+(j-i)//2
-            if nums[mid] > nums[mid + 1] and nums[mid] > nums[mid-1]:
-                return mid
-            elif nums[mid-1] < nums[mid]:
-                i=mid+1
+            return 0  # The first element is a peak
+        if nums[n - 1] > nums[n - 2]:
+            return n - 1  # The last element is a peak
+        
+        l, r = 0, n - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if mid > 0 and nums[mid] > nums[mid - 1] and mid < n - 1 and nums[mid] > nums[mid + 1]:
+                return mid  # Mid is a peak
+            elif mid > 0 and nums[mid - 1] > nums[mid]:
+                r = mid - 1  # Peak is to the left
             else:
-                j=mid-1
-        return None
+                l = mid + 1  # Peak is to the right
+        
+        # This return statement is a fallback and theoretically should never be reached because
+        # the while loop should always return from inside. 
+        return l
