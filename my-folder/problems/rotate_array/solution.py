122ef6879ee0d47732a09efa06e24091225d3547
OpenFusionUSA <web.developer@openfusion.us>
2024-02-13 17:26:20 +0000

    Sync LeetCode submission - Rotate Array (python3)

--- my-folder/problems/rotate_array/solution.py
+++ my-folder/problems/rotate_array/solution.py
@@ -1,15 +1,18 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
         n=len(nums)
         k%=n
-        start=count=0
+        start,count=0,0
         while count<n:
-            current,prev=start,nums[start]
+            current,value=start,nums[start]
             while True:
                 next_idx=(current+k)%n
-                nums[next_idx],prev=prev,nums[next_idx]
+                nums[next_idx],value=value,nums[next_idx]
                 current=next_idx
                 count+=1
-                if start == current:
+                if start==current:
                     break
             start+=1
