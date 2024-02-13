14fd049f17a2a12dbdea39536c35fb1bb60dae6e
OpenFusionUSA <web.developer@openfusion.us>
2024-02-13 16:45:19 +0000

    Sync LeetCode submission - Employee Bonus (mysql)

--- my-folder/problems/employee_bonus/solution.sql
+++ my-folder/problems/employee_bonus/solution.sql
@@ -1,6 +1,5 @@
-# Write your MySQL query statement below
-select name, bonus
-from Employee
-left join Bonus
-on Employee.empId = Bonus.empId
-where bonus < 1000 or bonus is null;
+select e.name, b.bonus
+from Employee e  
+left join Bonus b
+on e.empId = b.empId
+where ifnull(bonus, 0) < 1000;
