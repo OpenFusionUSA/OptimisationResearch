c4b92f889b67afcdc71a26c78da6209b8b45eb28
OpenFusionUSA <web.developer@openfusion.us>
2024-02-20 23:47:08 +0000

    Sync LeetCode submission - Students and Examinations (mysql)

--- my-folder/problems/students_and_examinations/solution.sql
+++ my-folder/problems/students_and_examinations/solution.sql
@@ -1,14 +1,18 @@
-SELECT s.student_id, 
-       s.student_name, 
-       sub.subject_name, 
-       COALESCE(e.exam_count, 0) as attended_exams
-FROM Students s
-CROSS JOIN Subjects sub
+# Write your MySQL query statement below
+select s.student_Id ,
+s.student_name , 
+sub.subject_name , 
+COALESCE(e.c,0) as attended_exams 
+from students s
+CROSS JOIN subjects sub
 LEFT JOIN (
-    SELECT student_id, 
-           subject_name, 
-           COUNT(*) as exam_count
-    FROM Examinations
-    GROUP BY student_id, subject_name
-) e ON s.student_id = e.student_id AND sub.subject_name = e.subject_name
-ORDER BY s.student_id, sub.subject_name;
+    select student_id,
+            subject_name,
+            COUNT(*) as c
+    from Examinations
+    group by student_id,
+             subject_name
+) e
+on e.student_id=s.student_id 
+and e.subject_name=sub.subject_name 
+order by s.student_Id,sub.subject_name ;
