0f0f5c99c91a1b71cbf3dc67efc96c8758f3b2dc
OpenFusionUSA <web.developer@openfusion.us>
2024-02-21 22:05:37 +0000

    Sync LeetCode submission - Confirmation Rate (mysql)

--- my-folder/problems/confirmation_rate/solution.sql
+++ my-folder/problems/confirmation_rate/solution.sql
@@ -1,4 +1,6 @@
-select Signups.user_id, round(avg(if(Confirmations.action ='confirmed', 1, 0)),2) as confirmation_rate
-from Signups
-left join Confirmations using (user_id)
-group by Signups.user_id
+# Write your MySQL query statement below
+select s.user_id, round(avg(if(c.action='confirmed',1,0)),2) as confirmation_rate 
+from Signups s
+left join Confirmations c
+ON s.user_id=c.user_id
+group by user_id;
