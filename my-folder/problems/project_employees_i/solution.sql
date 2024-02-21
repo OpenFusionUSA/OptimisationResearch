5425c391a4e06a6102a07e147be4d3c8dd8e7dbf
OpenFusionUSA <web.developer@openfusion.us>
2024-02-21 22:57:52 +0000

    Sync LeetCode submission - Project Employees I (mysql)

--- my-folder/problems/project_employees_i/solution.sql
+++ my-folder/problems/project_employees_i/solution.sql
@@ -1,6 +1,8 @@
 # Write your MySQL query statement below
-select project_id, round(avg(experience_years),2) as average_years
-from Project
-left join Employee
-on Project.employee_id = Employee.employee_id
-group by project_id
+select p.project_id,
+ROUND(AVG(e.experience_years),2) 
+as average_years
+from project p
+left join Employee e
+on p.employee_id=e.employee_id
+group by project_id;
