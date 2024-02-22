5c4327b18fbfabaf1bffaf2da78409ff96a3dffc
OpenFusionUSA <web.developer@openfusion.us>
2024-02-22 22:52:47 +0000

    Sync LeetCode submission - Monthly Transactions I (mysql)

--- my-folder/problems/monthly_transactions_i/solution.sql
+++ my-folder/problems/monthly_transactions_i/solution.sql
@@ -1,13 +1,10 @@
 # Write your MySQL query statement below
-
-#select SUM(amount), COUNT(*) from Transactions where state='approved' group by MONTH(trans_date);
-SELECT DATE_FORMAT(trans_date, '%Y-%m') AS month, country, count(*) as trans_count,
-sum(state = 'approved') as approved_count, sum(amount) as trans_total_amount,
-sum(case when state = 'approved' then amount else 0 end) as approved_total_amount
+select DATE_FORMAT(trans_date,'%Y-%m') as month,
+country,
+COUNT(*) as trans_count,
+SUM(IF(state='approved',1,0)) as approved_count,
+SUM(amount) AS trans_total_amount,
+SUM(IF(state='approved',amount,0)) as approved_total_amount
 from Transactions
-GROUP BY month, country;
-
-
-
-
-
+group by month,country
+order by month;
