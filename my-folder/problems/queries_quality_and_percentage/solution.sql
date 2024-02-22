35c2d9057692d872a74283e8f74e8499c274892f
OpenFusionUSA <web.developer@openfusion.us>
2024-02-22 22:40:27 +0000

    Sync LeetCode submission - Queries Quality and Percentage (mysql)

--- my-folder/problems/queries_quality_and_percentage/solution.sql
+++ my-folder/problems/queries_quality_and_percentage/solution.sql
@@ -1,5 +1,7 @@
 # Write your MySQL query statement below
-select query_name, round(avg(rating/position),2) as quality, 
-round((sum(case when rating < 3 then 1 else 0 end)/count(*) *100),2) as poor_query_percentage
-from queries
-group by query_name;
+select query_name,
+ROUND(AVG(rating/position),2) as quality,
+ROUND(SUM(IF(rating<3,1,0))*100/COUNT(*),2) as poor_query_percentage 
+from Queries 
+WHERE query_name is not Null
+ group by query_name;
