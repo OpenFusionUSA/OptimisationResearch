29a071f3c7c2f4aa21aa4f0c12efab5b529d07b8
OpenFusionUSA <web.developer@openfusion.us>
2024-02-23 22:16:26 +0000

    Sync LeetCode submission - Primary Department for Each Employee (mysql)

--- my-folder/problems/primary_department_for_each_employee/solution.sql
+++ my-folder/problems/primary_department_for_each_employee/solution.sql
@@ -1,8 +1,12 @@
 # Write your MySQL query statement below
-select employee_id, department_id
-from Employee 
-where primary_flag = 'Y' 
-union
-select employee_id, department_id from Employee
-group by employee_id
-having count(department_id) = 1;
+
+
+select employee_id,department_id
+from employee
+where employee_id in 
+(
+    select employee_id 
+    from employee
+    group by employee_id
+    having count(distinct department_id)=1
+) or primary_flag='Y';
