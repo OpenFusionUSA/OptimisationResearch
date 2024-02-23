40f4ba1963b64ff1b29377a0f7908e6ffab7b034
OpenFusionUSA <web.developer@openfusion.us>
2024-02-23 04:53:52 +0000

    Sync LeetCode submission - Game Play Analysis IV (mysql)

--- my-folder/problems/game_play_analysis_iv/solution.sql
+++ my-folder/problems/game_play_analysis_iv/solution.sql
@@ -3,4 +3,18 @@
 select ROUND( COUNT(DISTINCT A1.player_id)/(select count(DISTINCT player_id) from Activity) ,2) as fraction
 from Activity A1 
 join (select player_id,MIN(event_date) as event_date from Activity group by player_id) A2
-ON DATEDIFF(A1.event_date,A2.event_date)=1 and A1.player_id=A2.player_id;
+ON A1.player_id=A2.player_id and DATEDIFF(A1.event_date,A2.event_date)=1;  
+
+
+-- SELECT 
+--     ROUND(
+--         COUNT(DISTINCT A1.player_id) / 
+--         (SELECT COUNT(DISTINCT player_id) FROM Activity), 
+--         2
+--     ) AS fraction
+-- FROM 
+--     Activity A1
+-- JOIN 
+--     (SELECT player_id, MIN(event_date) AS event_date FROM Activity GROUP BY player_id) A2
+-- ON 
+--     A1.player_id = A2.player_id AND DATEDIFF(A1.event_date, A2.event_date) = 1;
