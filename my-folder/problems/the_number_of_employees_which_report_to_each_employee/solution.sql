9ad9389b35cb55666194d416c0ff30bd22a14bf1
OpenFusionUSA <web.developer@openfusion.us>
2024-02-23 22:03:16 +0000

    Sync LeetCode submission - The Number of Employees Which Report to Each Employee (mysql)

--- my-folder/problems/the_number_of_employees_which_report_to_each_employee/solution.sql
+++ my-folder/problems/the_number_of_employees_which_report_to_each_employee/solution.sql
@@ -1,7 +1,11 @@
 # Write your MySQL query statement below
-select a1.employee_id, a1.name, count(a2.reports_to) as reports_count, round(avg(a2.age*1.00),0) as average_age
-from Employees a1
-join Employees a2
-on a1.employee_id = a2.reports_to
-group by employee_id,a1.name
-order by a1.employee_id;
+select e1.employee_id, 
+e1.name,
+COUNT(e2.employee_id) as reports_count,
+ROUND(AVG(e2.age)) as average_age
+from employees e1
+left join employees e2
+on e1.employee_id=e2.reports_to
+where e2.employee_id is not null
+group by e1.employee_id
+order by e1.employee_id;
