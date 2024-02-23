3c081a38ed74cfa65e3c6e99bff0d5c167980a7b
OpenFusionUSA <web.developer@openfusion.us>
2024-02-23 17:54:47 +0000

    Sync LeetCode submission - Classes More Than 5 Students (mysql)

--- my-folder/problems/classes_more_than_5_students/solution.sql
+++ my-folder/problems/classes_more_than_5_students/solution.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select class
-from Courses
-group by class
-having count(class) >= 5;
+select class 
+from courses 
+group by class 
+having count(class)>4;
