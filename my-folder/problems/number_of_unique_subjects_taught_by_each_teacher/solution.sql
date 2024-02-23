0f89cb17806db5814927227f2cecf7fd434063a4
OpenFusionUSA <web.developer@openfusion.us>
2024-02-23 05:10:59 +0000

    Sync LeetCode submission - Number of Unique Subjects Taught by Each Teacher (mysql)

--- my-folder/problems/number_of_unique_subjects_taught_by_each_teacher/solution.sql
+++ my-folder/problems/number_of_unique_subjects_taught_by_each_teacher/solution.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-SELECT teacher_id, COUNT(DISTINCT subject_id) AS cnt
-from Teacher
-group by teacher_id;
+select teacher_id,count(DISTINCT(subject_id)) as cnt
+ from teacher 
+ group by teacher_id;
