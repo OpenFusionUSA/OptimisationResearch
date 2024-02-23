d7ece5ab4643eccf6833024bc550002a00966824
OpenFusionUSA <web.developer@openfusion.us>
2024-02-23 22:28:34 +0000

    Sync LeetCode submission - Triangle Judgement (mysql)

--- my-folder/problems/triangle_judgement/solution.sql
+++ my-folder/problems/triangle_judgement/solution.sql
@@ -1,4 +1,10 @@
 # Write your MySQL query statement below
-select x, y, z, 
-case when x + y > z and x + z > y and y + z > x then 'Yes' else 'No' end as triangle
-from Triangle
+select x,y,z,
+CASE WHEN x+y>z 
+and y+z>x
+and x+z>y
+then 'Yes'
+else 'No'
+end 
+as Triangle 
+from Triangle;
