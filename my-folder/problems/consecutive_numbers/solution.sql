f90baa3529c49067aee45641345ac0064e3ceeb0
OpenFusionUSA <web.developer@openfusion.us>
2024-02-23 22:34:40 +0000

    Sync LeetCode submission - Consecutive Numbers (mysql)

--- my-folder/problems/consecutive_numbers/solution.sql
+++ my-folder/problems/consecutive_numbers/solution.sql
@@ -1,6 +1,5 @@
 # Write your MySQL query statement below
 select distinct a1.num as ConsecutiveNums
-from Logs a1, logs a2, logs a3
-where a1.id = a2.id - 1 and a2.id = a3.id - 1
-and a1.num = a2.num and a2.num = a3.num;
-
+from Logs a1, Logs a2, Logs a3
+where a1.id=a2.id-1 and a2.id=a3.id-1
+and a1.num=a2.num and a2.num=a3.num;
