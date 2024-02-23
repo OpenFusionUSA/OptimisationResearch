dfb93b91bc8dd8820df31c1ebe5834772373f203
OpenFusionUSA <web.developer@openfusion.us>
2024-02-23 18:00:42 +0000

    Sync LeetCode submission - Biggest Single Number (mysql)

--- my-folder/problems/biggest_single_number/solution.sql
+++ my-folder/problems/biggest_single_number/solution.sql
@@ -1,7 +1,3 @@
 # Write your MySQL query statement below
-select max(num) as num
-from 
-(select num
-from MyNumbers
-group by num
-having count(num) = 1) as t;
+select max(num) as num from (select num from mynumbers 
+group by num having count(num)=1) as uniq;
