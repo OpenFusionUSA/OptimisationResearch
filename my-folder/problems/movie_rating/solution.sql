9b3baeeeebca479dbef198f1152ce0cec301b834
OpenFusionUSA <web.developer@openfusion.us>
2024-02-25 18:52:34 +0000

    Sync LeetCode submission - Movie Rating (mysql)

--- my-folder/problems/movie_rating/solution.sql
+++ my-folder/problems/movie_rating/solution.sql
@@ -1,20 +1,21 @@
 # Write your MySQL query statement below
-select * from (select u.name as results from users u
-join (select user_id, count(user_id) as count from movierating
-      group by user_id
-      ) as mr
-      on u.user_id = mr.user_id
-      order by mr.count desc, u.name limit 1) 
-as user_query
-union all 
-select * from 
-    (select m.title as results from movies m 
-    join 
-        (
-            select movie_id, avg(rating) as avg_rating from movierating 
-            where created_at >= '2020-02-01' and created_at <= '2020-02-28' 
-            group by movie_id
-        ) as sq 
-        on m.movie_id = sq.movie_id 
-    order by sq.avg_rating desc, m.title limit 1) 
-as movie_query      
+(
+    select u.name AS results
+from movierating m
+left join users u
+on m.user_id=u.user_id
+group by m.user_id
+order by count(*) desc, u.name limit 1
+)
+UNION ALL
+( 
+    select title as results from 
+movies m 
+left join (
+    select movie_id,AVG(rating) as avg_rating from movierating
+    where MONTH(created_at)=2 AND YEAR(created_at)=2020
+    group by movie_id
+) t
+on m.movie_id=t.movie_id 
+order by avg_rating desc, title limit 1
+);
