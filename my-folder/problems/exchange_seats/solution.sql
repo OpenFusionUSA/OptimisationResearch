45a6dd15c17b083e992e7b232e4490e4f98ae658
OpenFusionUSA <web.developer@openfusion.us>
2024-02-25 18:38:58 +0000

    Sync LeetCode submission - Exchange Seats (mysql)

--- my-folder/problems/exchange_seats/solution.sql
+++ my-folder/problems/exchange_seats/solution.sql
@@ -1,8 +1,13 @@
 # Write your MySQL query statement below
-select case when s.id % 2 != 0 and s.id = (select count(*) from Seat) then s.id
-            when s.id % 2 = 0 then s.id - 1
-            else s.id +1
-            end as id, 
-            student
-from Seat s
-order by id            
+
+
+
+select 
+(case 
+when id=(select Max(id) from seat) and id%2=1 then id
+when id%2=0 then id-1
+when id%2=1 then id+1
+end
+) as id, student 
+from seat 
+order by id;
