427f08f10ea1f2d533cc71220696cd5b75443278
OpenFusionUSA <web.developer@openfusion.us>
2024-02-26 16:19:36 +0000

    Sync LeetCode submission - Friend Requests II: Who Has the Most Friends (mysql)

--- my-folder/problems/friend_requests_ii:_who_has_the_most_friends/solution.sql
+++ my-folder/problems/friend_requests_ii:_who_has_the_most_friends/solution.sql
@@ -1,10 +1,10 @@
 # Write your MySQL query statement below
-select id, count(*) as num
-from( 
-    select requester_id as id from RequestAccepted
+with conn as 
+(
+    select requester_id as id from RequestAccepted 
     union all
-    select accepter_id as id from RequestAccepted
-) as friends_count
-group by id
-order by num desc
-limit 1;
+    select accepter_id  as id from RequestAccepted 
+)
+
+select id,count(*) as num from conn group by id 
+order by num desc limit 1;
