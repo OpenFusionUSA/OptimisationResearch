2e1195a49bff56d610ec6844157a1788b0a73104
OpenFusionUSA <web.developer@openfusion.us>
2024-02-26 16:15:45 +0000

    Sync LeetCode submission - Restaurant Growth (mysql)

--- my-folder/problems/restaurant_growth/solution.sql
+++ my-folder/problems/restaurant_growth/solution.sql
@@ -1,22 +1,11 @@
-# Write your MySQL query statement below
-SELECT
-    visited_on,
-    (
-        SELECT SUM(amount)
-        FROM customer
-        WHERE visited_on BETWEEN DATE_SUB(c.visited_on, INTERVAL 6 DAY) AND c.visited_on
-    ) AS amount,
-    ROUND(
-        (
-            SELECT SUM(amount) / 7
-            FROM customer
-            WHERE visited_on BETWEEN DATE_SUB(c.visited_on, INTERVAL 6 DAY) AND c.visited_on
-        ),
-        2
-    ) AS average_amount
-FROM customer c
-WHERE visited_on >= (
-        SELECT DATE_ADD(MIN(visited_on), INTERVAL 6 DAY)
-        FROM customer
-    )
-GROUP BY visited_on;
+WITH cust as (
+SELECT c.visited_on,
+       SUM(c.amount) OVER (ORDER BY c.visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS amount,
+       AVG(c.amount) OVER (ORDER BY c.visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS average_amount
+FROM 
+( select visited_on,SUM(amount) as amount from customer group by visited_On) c
+ORDER BY c.visited_on
+)
+select visited_on,amount,ROUND(average_amount,2) as average_amount  from cust 
+WHERE DATEDIFF(visited_on, (SELECT MIN(visited_on) FROM customer)) >= 6
+;
