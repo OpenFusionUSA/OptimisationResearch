50f5d4d954af18861d358c8a468527d7d12df960
OpenFusionUSA <web.developer@openfusion.us>
2024-03-24 20:21:47 +0000

    Sync LeetCode submission - Number of Connected Components in an Undirected Graph (python3)

--- my-folder/problems/number_of_connected_components_in_an_undirected_graph/solution.py
+++ my-folder/problems/number_of_connected_components_in_an_undirected_graph/solution.py
@@ -1,32 +1,31 @@
-
-        
 class UnionFind:
-    def __init__(self, size):
-        self.root = [i for i in range(size)]
-
-    def find(self, x):
+    def __init__(self,n):
+        self.root=[i for i in range(n)]
+        self.rank=[1 for _ in range(n)]
+    def find(self,x):
+        if x==self.root[x]:
+            return x
+        self.root[x]=self.find(self.root[x])
         return self.root[x]
-		
-    def union(self, x, y):
-        rootX = self.find(x)
-        rootY = self.find(y)
-        if rootX != rootY:
-            for i in range(len(self.root)):
-                if self.root[i] == rootY:
-                    self.root[i] = rootX
-
-    def connected(self, x, y):
-        return self.find(x) == self.find(y)
-    def noOfRoots(self):
-        print(self.root)
-        return len(set(self.root))
-    
-class Solution(object):
-    def countComponents(self, n, edges):
-        tree=UnionFind(n)
-        for edge in edges:
-            tree.union(edge[0],edge[1])
-        return tree.noOfRoots()        
-
-
-        
+    def union(self,x,y):
+        rootx=self.find(x)
+        rooty=self.find(y)
+        if rootx!=rooty:
+            if self.rank[rootx]>self.rank[rooty]:
+                self.root[rooty]=rootx
+            elif self.rank[rootx]<self.rank[rooty]:
+                self.root[rootx]=rooty
+            else:
+                self.root[rooty]=rootx
+                self.rank[rootx]+=1
+    def isConnected(self,x,y):
+        return self.root[x]==self.root[y]
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        uf=UnionFind(n)
+        for s,d in edges:
+            uf.union(s,d)
+        p=set()
+        for i in range(n):
+            p.add(uf.find(i))
+        return len(p)
