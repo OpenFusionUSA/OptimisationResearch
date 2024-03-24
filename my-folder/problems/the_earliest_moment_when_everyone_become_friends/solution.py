b5de009397263c58999d7dc36d7dfdfa0db6c8c3
OpenFusionUSA <web.developer@openfusion.us>
2024-03-24 22:00:38 +0000

    Sync LeetCode submission - The Earliest Moment When Everyone Become Friends (python3)

--- my-folder/problems/the_earliest_moment_when_everyone_become_friends/solution.py
+++ my-folder/problems/the_earliest_moment_when_everyone_become_friends/solution.py
@@ -1,39 +1,26 @@
 class UnionFind:
-    def __init__(self, size):
-        self.root = [i for i in range(size)]
-
-    def find(self, x):
+    def __init__(self,n):
+        self.root=[i for i in range(n)]
+        self.rank=[1 for _ in range(n)]
+    def find(self,x):
         return self.root[x]
-		
-    def union(self, x, y):
-        rootX = self.find(x)
-        rootY = self.find(y)
-        if rootX != rootY:
+    def union(self,x,y):
+        rootx=self.find(x)
+        rooty=self.find(y)
+        if rootx!=rooty:
             for i in range(len(self.root)):
-                if self.root[i] == rootY:
-                    self.root[i] = rootX
-
-    def connected(self, x, y):
-        return self.find(x) == self.find(y)
-    def noOfRoots(self):
+                if self.root[i]==rooty:
+                    self.root[i]=rootx
+    def isconnected(self,x,y):
+        return self.find(x)==self.find(y)
+    def noofRoots(self):
         return len(set(self.root))
-
-class Solution(object):
-    
-    def earliestAcq(self, logs, n):
-        """
-        :type logs: List[List[int]]
-        :type n: int
-        :rtype: int
-        """
-        tree=UnionFind(n)
-        logs=sorted(logs, key=lambda x: x[0])
-        for log in logs:
-            tree.union(log[1],log[2])
-            if tree.noOfRoots() == 1:
-                return log[0]
+class Solution:
+    def earliestAcq(self, logs: List[List[int]], n: int) -> int:
+        uf=UnionFind(n)
+        logs=sorted(logs,key=lambda x:x[0])
+        for t,s,d in logs:
+            uf.union(s,d)
+            if uf.noofRoots()==1:
+                return t
         return -1
-        
-                
-        
-        
