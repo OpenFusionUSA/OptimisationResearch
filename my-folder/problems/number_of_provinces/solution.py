5e69bdce112549b85fc44e119d98353127f5cd5a
OpenFusionUSA <web.developer@openfusion.us>
2024-03-24 18:07:36 +0000

    Sync LeetCode submission - Number of Provinces (python3)

--- my-folder/problems/number_of_provinces/solution.py
+++ my-folder/problems/number_of_provinces/solution.py
@@ -1,43 +1,34 @@
 class UnionFind:
-    def __init__(self, size):
-        self.root = [i for i in range(size)]
-
-    def find(self, x):
+    def __init__(self,n):
+        self.root=[i for i in range(n)]
+        self.rank=[1 for _ in range(n)]
+    def find(self,x):
+        if x==self.root[x]:
+            return x
+        self.root[x]=self.find(self.root[x])
         return self.root[x]
-		
-    def union(self, x, y):
-        rootX = self.find(x)
-        rootY = self.find(y)
-        if rootX != rootY:
-            for i in range(len(self.root)):
-                if self.root[i] == rootY:
-                    self.root[i] = rootX
-
-    def connected(self, x, y):
-        return self.find(x) == self.find(y)
-    def distinctLen(self):
-        print(self.root)
-        return len(set(self.root))
-
-
-
-class Solution(object):
-    def findCircleNum(self, isConnected):
-        """
-        :type isConnected: List[List[int]]
-        :rtype: int
-        """
+    def union(self,x,y):
+        rootx=self.find(x)
+        rooty=self.find(y)
+        if (rootx!=rooty):
+            if self.rank[rootx]>self.rank[rooty]:
+                self.root[rooty]=rootx
+            elif self.rank[rootx]<self.rank[rooty]:
+                self.root[rootx]=rooty
+            else:
+                self.root[rooty]=rootx
+                self.rank[rootx]+=1
+    def isConnected(self,x,y):
+        return self.find(x)==self.find(y)
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
         n=len(isConnected)
-        network = UnionFind(n)
+        uf=UnionFind(n)
+        numberofcomponents=n
         for i in range(n):
-            for j in range(n):
-                if isConnected[i][j]==1:
-                    network.union(i,j)
-        return network.distinctLen()
-    
-    
-
-
-
-
+            for j in range(i+1,n):
+                if isConnected[i][j]==1 and uf.find(i)!=uf.find(j):
+                    numberofcomponents-=1
+                    uf.union(i,j)
+        return numberofcomponents
         
