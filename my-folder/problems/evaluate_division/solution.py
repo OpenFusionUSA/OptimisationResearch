eddb5896d91f9c578cf32bc8c58169147e6a01f4
OpenFusionUSA <web.developer@openfusion.us>
2024-03-25 02:04:40 +0000

    Sync LeetCode submission - Evaluate Division (python)

--- my-folder/problems/evaluate_division/solution.py
+++ my-folder/problems/evaluate_division/solution.py
@@ -6,39 +6,34 @@ class Solution(object):
         :type queries: List[List[str]]
         :rtype: List[float]
         """
-        dic = {}
+        dic={}
         def find(x):
             if x not in dic:
                 dic[x]=(x,1)
-            grp_id,wt=dic[x]
-            if x!=grp_id:
-                new_grp_id,new_weight=find(grp_id)
-                dic[x]=(new_grp_id,new_weight*wt)
+            root,weight=dic[x]
+            if root!=x:
+                new_root,new_weight=find(root)
+                dic[x]=(new_root,weight*new_weight)
             return dic[x]
         def union(x,y,value):
             g_1,w_1=find(x)
             g_2,w_2=find(y)
             if g_1!=g_2:
-                dic[g_1]=(g_2,(value*w_2)/w_1)
-        
+                dic[g_1]=(g_2,((value*w_2)/w_1))
         res=[]
-
         for (div,dis),value in zip(equations,values):
             union(div,dis,value)
-        print(dic)
-        for div,dis in queries:
+        for (div,dis) in queries:
             if div not in dic or dis not in dic:
-                res.append(-1.0)
+                res.append(-1)
                 continue
-            if div == dis:
-                res.append(1.0)
+            if div==dis:
+                res.append(1)
                 continue
             g_1,w_1=find(div)
             g_2,w_2=find(dis)
-            if g_1==g_2:
-                res.append(w_1/w_2)
-            else:
+            if g_1!=g_2:
                 res.append(-1)
+                continue
+            res.append(w_1/w_2)
         return res
-
-        
