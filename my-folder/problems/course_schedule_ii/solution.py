f7054f2a1d3434f629b121e037fb8137f50427c1
OpenFusionUSA <web.developer@openfusion.us>
2024-03-30 17:17:18 +0000

    Sync LeetCode submission - Course Schedule II (python3)

--- my-folder/problems/course_schedule_ii/solution.py
+++ my-folder/problems/course_schedule_ii/solution.py
@@ -1,27 +1,24 @@
-class Solution(object):
-    def findOrder(self, numCourses, prerequisites):
-        """
-        :type numCourses: int
-        :type prerequisites: List[List[int]]
-        :rtype: List[int]
-        """
-        pre=[[] for _ in range(numCourses)]
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph={i:[] for i in range(numCourses)}
         for a,b in prerequisites:
-            pre[a].append(b)
+            graph[a].append(b)
         out=[]
-        visit,cycle=set(),set()
+        visited,cycle=set(),set()
         def dfs(node):
             if node in cycle:
                 return False
-            if node in visit:
+            if node in visited:
                 return True
             cycle.add(node)
-            for c in pre[node]:
-                if not dfs(c): return False
+            for adjnode in graph[node]:
+                if not dfs(adjnode):
+                    return False
             cycle.remove(node)
-            visit.add(node)
+            visited.add(node)
             out.append(node)
             return True
-        for c in range(numCourses):
-            if not dfs(c): return []
+        for i in range(numCourses):
+            if not dfs(i):
+                return []
         return out
