b21b0446241a10c424487a2711eb30c36de761ae
OpenFusionUSA <web.developer@openfusion.us>
2024-03-30 22:51:14 +0000

    Sync LeetCode submission - Sort Linked List Already Sorted Using Absolute Values (python3)

---
+++ my-folder/problems/sort_linked_list_already_sorted_using_absolute_values/solution.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortLinkedList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev=head
+        node=head.next
+        while node:
+            next_node=node.next
+            if node.val<0:
+                node.next=head
+                head=node
+                prev.next=next_node
+            else:
+                prev=prev.next
+            node=next_node
+        return head
