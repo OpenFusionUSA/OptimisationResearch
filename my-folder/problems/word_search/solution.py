3133deffa535dec6eee62c8ff53c2e2a081596c9
OpenFusionUSA <web.developer@openfusion.us>
2024-04-01 01:23:56 +0000

    Sync LeetCode submission - Word Search (python3)

--- my-folder/problems/word_search/solution.py
+++ my-folder/problems/word_search/solution.py
@@ -1,19 +1,20 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        m=len(board)
-        n=len(board[0])
-        path=set()
+        n=len(word)
+        bn=len(board)
+        bm=len(board[0])
+        visited=set()
         def dfs(x,y,i):
-            if i==len(word):
+            if i==n:
                 return True
-            if x<0 or x>=m or y<0 or y>=n or board[x][y]!=word[i] or (x,y) in path:
+            if x<0 or x>=bn or y<0 or y>=bm or word[i]!=board[x][y] or (x,y) in visited:
                 return False
-            path.add((x,y))
-            res= dfs(x+1,y,i+1) or dfs(x-1,y,i+1) or dfs(x,y+1,i+1) or dfs(x,y-1,i+1)
-            path.remove((x,y))
-            return res
-        for i in range(m):
-            for j in range(n):
+            visited.add((x,y))
+            ret=dfs(x+1,y,i+1) or dfs(x,y+1,i+1) or dfs(x,y-1,i+1) or dfs(x-1,y,i+1)
+            visited.remove((x,y))
+            return ret
+        for i in range(bn):
+            for j in range(bm):
                 if dfs(i,j,0):
                     return True
         return False
