739807742dfa29eef25ffb01128620c18ee64a1b
OpenFusionUSA <web.developer@openfusion.us>
2024-04-01 08:13:51 +0000

    Sync LeetCode submission - Maximum Product Subarray (python3)

--- my-folder/problems/maximum_product_subarray/solution.py
+++ my-folder/problems/maximum_product_subarray/solution.py
@@ -1,15 +1,14 @@
-class Solution(object):
-    def maxProduct(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        max_so_far=nums[0]
-        min_so_far=nums[0]
-        result=max(max_so_far,min_so_far)
-        for n in nums[1:]:
-            temp_max=max(max_so_far*n,n,min_so_far*n)
-            min_so_far=min(n,max_so_far*n,min_so_far*n)
-            max_so_far=temp_max
-            result=max(max_so_far,result)
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        if len(nums)==0:
+            return 0
+        mi=nums[0]
+        ma=nums[0]
+        result=ma
+        for i in range(1,len(nums)):
+            curr=nums[i]
+            temp_max=max(curr,curr*ma,mi*curr)
+            mi=min(curr,curr*ma,mi*curr)
+            ma=temp_max
+            result=max(ma,result)
         return result
