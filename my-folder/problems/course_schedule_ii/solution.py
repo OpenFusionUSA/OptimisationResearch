9ea1b6a3d87e6ce7363a95ad4b7026dfc75f8039
OpenFusionUSA <web.developer@openfusion.us>
2024-04-06 19:19:26 +0000

    Sync LeetCode submission - Course Schedule II (python3)

--- my-folder/problems/course_schedule_ii/solution.py
+++ my-folder/problems/course_schedule_ii/solution.py
@@ -1,25 +1,24 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         graph=collections.defaultdict(list)
+        indegre=[0]*numCourses
         for (a,b) in prerequisites:
             graph[a].append(b)
-        visited=set()
-        cycle=set()
-        out=[]
-        def dfs(node):
-            if node in cycle:
-                return False
-            if node in visited:
-                return True
-            cycle.add(node)
-            for adjnode in graph[node]:
-                if not dfs(adjnode):
-                    return False
-            cycle.remove(node)
-            visited.add(node)
-            out.append(node)
-            return True
+            indegre[b]+=1
+        q=collections.deque()
         for i in range(numCourses):
-            if not dfs(i):
-                return []
-        return out
+            if indegre[i]==0:
+                q.append(i)
+        processorder=[]
+        processcount=0
+        while q:
+            node=q.popleft()
+            for adjnode in graph[node]:
+                indegre[adjnode]-=1
+                if indegre[adjnode]<=0:
+                    q.append(adjnode)
+            processorder.append(node)
+            processcount+=1
+        if processcount==numCourses:
+            return processorder[::-1]
+        return []
