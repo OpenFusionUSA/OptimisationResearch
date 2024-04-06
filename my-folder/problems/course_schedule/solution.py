d9c7c2fd5aecd956e687b84ae856ab9d42b9967e
OpenFusionUSA <web.developer@openfusion.us>
2024-04-06 19:18:26 +0000

    Sync LeetCode submission - Course Schedule (python3)

--- my-folder/problems/course_schedule/solution.py
+++ my-folder/problems/course_schedule/solution.py
@@ -1,21 +1,25 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        pre=[[] for _ in range(numCourses)]
-        for a,b in prerequisites:
-            pre[a].append(b)
-        visited=set()
-        def dfs(node):
-            if node in visited:
-                #### loop 
-                return False
-            if len(pre[node])==0:
-                return True
-            visited.add(node)
-            for c in pre[node]:
-                if not dfs(c): return False
-            visited.remove(node)
-            pre[node]=[]
-            return True
+        graph=collections.defaultdict(list)
+        indegre=[0]*numCourses
+        for (a,b) in prerequisites:
+            graph[a].append(b)
+            indegre[b]+=1
+        q=collections.deque()
         for i in range(numCourses):
-            if not dfs(i): return False
-        return True
+            if indegre[i]==0:
+                q.append(i)
+        processorder=[]
+        processcount=0
+        while q:
+            node=q.popleft()
+            for adjnode in graph[node]:
+                indegre[adjnode]-=1
+                if indegre[adjnode]<=0:
+                    q.append(adjnode)
+            processorder.append(node)
+            processcount+=1
+        print(processorder[::-1])
+        if processcount==numCourses:
+            return True
+        return False
