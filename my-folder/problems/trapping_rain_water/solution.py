64e94b0f4ded96d8207bb2b7bad5182edd073983
OpenFusionUSA <web.developer@openfusion.us>
2024-04-12 16:03:23 +0000

    Sync LeetCode submission - Trapping Rain Water (python)

--- my-folder/problems/trapping_rain_water/solution.py
+++ my-folder/problems/trapping_rain_water/solution.py
@@ -1,24 +1,16 @@
 class Solution(object):
     def trap(self, height):
-        if not height:
-            return 0
-
-        n = len(height)
-        left_max, right_max = [0] * n, [0] * n
-        left_max[0] = height[0]
-        right_max[n - 1] = height[n - 1]
-
-        # Fill left_max
-        for i in range(1, n):
-            left_max[i] = max(height[i], left_max[i - 1])
-
-        # Fill right_max
-        for i in range(n - 2, -1, -1):
-            right_max[i] = max(height[i], right_max[i + 1])
-
-        # Calculate trapped water
-        water_trapped = 0
-        for i in range(n):
-            water_trapped += min(left_max[i], right_max[i]) - height[i]
-
-        return water_trapped
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        n=len(height)
+        left_min,right_min=[0]*n,[0]*n
+        left_min[0],right_min[n-1]=height[0],height[n-1]
+        for i in range(1,n):
+            left_min[i]=max(left_min[i-1],height[i])
+            right_min[n-i-1]=max(right_min[n-i],height[n-i-1])
+        ans=0
+        for i in range(1,n-1):
+            ans+=(min(left_min[i],right_min[i])-height[i])
+        return ans
