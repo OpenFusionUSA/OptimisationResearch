10ab42e1f8097f3bf1a6aa0aaffc5de8a298d705
OpenFusionUSA <web.developer@openfusion.us>
2024-05-09 03:55:34 +0000

    Sync LeetCode submission - Maximize Happiness of Selected Children (python3)

---
+++ my-folder/problems/maximize_happiness_of_selected_children/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
+        happiness.sort(reverse=True)
+        ans=0
+        for i in range(k):
+            if happiness[i]-i>0:
+                ans+=happiness[i]-i
+            else:
+                break
+        return ans
