8a5ce313020904f1dad6020ff20321caa3a604b3
OpenFusionUSA <web.developer@openfusion.us>
2024-05-12 03:20:01 +0000

    Sync LeetCode submission - Permutation in String (python3)

--- my-folder/problems/permutation_in_string/solution.py
+++ my-folder/problems/permutation_in_string/solution.py
@@ -1,34 +1,33 @@
-class Solution(object):
-    def checkInclusion(self, s1, s2):
-        """
-        :type s1: str
-        :type s2: str
-        :rtype: bool
-        """
-        s1map={}
-        for s in s1:
-            s1map[s]=s1map.get(s,0)+1
-        s2map={}
-        m=len(s1)
-        n=len(s2)
-        if n<m:
-            return False
-        i=0
-        j=m
-        for k in range(m):
-            s2map[s2[k]]=s2map.get(s2[k],0)+1
-        if s1map==s2map:
-                return True
-        while j<n:
-            s2map[s2[j]]=s2map.get(s2[j],0)+1
-            if s2map[s2[i]] == 1:
-                del s2map[s2[i]]
-            else:
-                s2map[s2[i]] -= 1
-            if s1map==s2map:
-                return True
-            i+=1
-            j+=1
-            
-        return False
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        def dictcont(d2,d1):
+            for k,v in d1.items():
+                if not(k in d2 and d2[k]>=v):
+                    return False
+            return True
         
+        ds1 = {}
+        for c in s1:
+            if c not in ds1: ds1[c]=0
+            ds1[c] += 1
+        
+        ds2 = {}
+        n=len(s2)
+        start = 0
+        for i in range(n):
+            if s2[i] in ds1:
+                if s2[i] not in ds2: ds2[s2[i]] = 0
+                ds2[s2[i]] += 1
+                # print(ds1)
+                # print(ds2)
+                # print(dictcont(ds1,ds2))
+                while not dictcont(ds1,ds2):
+                    ds2[s2[start]]-=1
+                    if ds2[s2[start]]==0: del ds2[s2[start]]
+                    start+=1
+                # print(start,i)
+                if dictcont(ds2,ds1): return True
+            else:
+                ds2 = {}
+                start = i+1
+        return False
