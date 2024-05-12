d36b4b5a231a8af749e7dd8beefc802abebee9e1
OpenFusionUSA <web.developer@openfusion.us>
2024-05-12 21:46:25 +0000

    Sync LeetCode submission - Lowest Common Ancestor of a Binary Tree (python3)

--- my-folder/problems/lowest_common_ancestor_of_a_binary_tree/solution.py
+++ my-folder/problems/lowest_common_ancestor_of_a_binary_tree/solution.py
@@ -11,8 +11,8 @@ class Solution:
             return None
         if root==p or root==q:
             return root
-        left=self.lowestCommonAncestor(root.left,p,q)
-        right=self.lowestCommonAncestor(root.right,p,q)
+        left=self.lowestCommonAncestor(root.left, p, q)
+        right=self.lowestCommonAncestor(root.right, p, q)
         if left is not None and right is not None:
             return root
         else:
