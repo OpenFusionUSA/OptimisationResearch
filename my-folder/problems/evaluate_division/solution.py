9f011076c551f14ce79113fe4d2394ed8700d460
OpenFusionUSA <web.developer@openfusion.us>
2024-05-14 18:26:50 +0000

    Sync LeetCode submission - Evaluate Division (python)

--- my-folder/problems/evaluate_division/solution.py
+++ my-folder/problems/evaluate_division/solution.py
@@ -10,30 +10,29 @@ class Solution(object):
         def find(x):
             if x not in dic:
                 dic[x]=(x,1)
-            root,weight=dic[x]
-            if root!=x:
-                new_root,new_weight=find(root)
-                dic[x]=(new_root,weight*new_weight)
+            base,w=dic[x]
+            if base!=x:
+                new_root,w2=find(base)
+                dic[x]=(new_root,w2*w)
             return dic[x]
         def union(x,y,value):
-            g_1,w_1=find(x)
-            g_2,w_2=find(y)
-            if g_1!=g_2:
-                dic[g_1]=(g_2,((value*w_2)/w_1))
-        res=[]
-        for (div,dis),value in zip(equations,values):
-            union(div,dis,value)
-        for (div,dis) in queries:
-            if div not in dic or dis not in dic:
-                res.append(-1)
-                continue
-            if div==dis:
-                res.append(1)
-                continue
-            g_1,w_1=find(div)
-            g_2,w_2=find(dis)
-            if g_1!=g_2:
-                res.append(-1)
-                continue
-            res.append(w_1/w_2)
-        return res
+            b_x,w_x=find(x)
+            b_y,w_y=find(y)
+            if b_x!=b_y:
+                dic[b_x]=(b_y,(value*w_y)/w_x)
+        for [a,b],c in zip(equations,values):
+            union(a,b,c)
+        out=[]
+        for n,d in queries:
+            if n not in dic or d not in dic:
+                out.append(-1)
+            elif n==d:
+                out.append(1)
+            else:
+                b_x,w_x=find(n)
+                b_y,w_y=find(d)
+                if b_x!=b_y:
+                    out.append(-1)
+                else:
+                    out.append(w_x/w_y)
+        return out
