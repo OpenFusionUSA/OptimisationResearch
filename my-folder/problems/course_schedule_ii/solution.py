2abe377e628461337ab9da62cef5fe7f398ab484
OpenFusionUSA <web.developer@openfusion.us>
2024-05-19 05:29:33 +0000

    Sync LeetCode submission - Course Schedule II (python3)

--- my-folder/problems/course_schedule_ii/solution.py
+++ my-folder/problems/course_schedule_ii/solution.py
@@ -1,24 +1,20 @@
 class Solution:
-    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        graph=collections.defaultdict(list)
-        indegre=[0]*numCourses
-        for (a,b) in prerequisites:
-            graph[a].append(b)
-            indegre[b]+=1
-        q=collections.deque()
-        for i in range(numCourses):
-            if indegre[i]==0:
-                q.append(i)
-        processorder=[]
-        processcount=0
+    def findOrder(self, n: int, edges: List[List[int]]) -> List[int]:
+        adj = [[] for i in range(n)]
+        for e in edges:
+            adj[e[1]].append(e[0])
+        indeg = [0 for i in range(n)]
+        for lst in adj:
+            for node in lst:
+                indeg[node] += 1
+        q = deque()
+        for i in range(n):
+            if indeg[i]==0: q.append(i)
+        order = []
         while q:
-            node=q.popleft()
-            for adjnode in graph[node]:
-                indegre[adjnode]-=1
-                if indegre[adjnode]<=0:
-                    q.append(adjnode)
-            processorder.append(node)
-            processcount+=1
-        if processcount==numCourses:
-            return processorder[::-1]
-        return []
+            curnode = q.popleft()
+            order.append(curnode)
+            for neigh in adj[curnode]:
+                indeg[neigh]-=1
+                if indeg[neigh]==0: q.append(neigh)
+        return order if all(indeg[i]==0 for i in range(n)) else []
