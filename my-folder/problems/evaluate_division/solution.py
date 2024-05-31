18279e44aa3796a650ec00cef78b856e8c77023f
OpenFusionUSA <web.developer@openfusion.us>
2024-05-31 20:24:16 +0000

    Sync LeetCode submission - Evaluate Division (python)

--- my-folder/problems/evaluate_division/solution.py
+++ my-folder/problems/evaluate_division/solution.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 class Solution(object):
     def calcEquation(self, equations, values, queries):
         """
@@ -6,33 +8,36 @@ class Solution(object):
         :type queries: List[List[str]]
         :rtype: List[float]
         """
-        dic={}
-        def find(x):
-            if x not in dic:
-                dic[x]=(x,1)
-            base,w=dic[x]
-            if base!=x:
-                new_root,w2=find(base)
-                dic[x]=(new_root,w2*w)
-            return dic[x]
-        def union(x,y,value):
-            b_x,w_x=find(x)
-            b_y,w_y=find(y)
-            if b_x!=b_y:
-                dic[b_x]=(b_y,(value*w_y)/w_x)
-        for [a,b],c in zip(equations,values):
-            union(a,b,c)
-        out=[]
-        for n,d in queries:
-            if n not in dic or d not in dic:
-                out.append(-1)
-            elif n==d:
-                out.append(1)
+        def backtrackEvaluate(current, target, visited, product):
+            """ Helper function to evaluate division from current to target using DFS """
+            visited.add(current)
+            # Direct connection found
+            if target in graph[current]:
+                return product * graph[current][target]
+            # Search through neighbors
+            result = -1.0
+            for neighbor, value in graph[current].items():
+                if neighbor not in visited:
+                    result = backtrackEvaluate(neighbor, target, visited, product * value)
+                    if result != -1.0:
+                        break
+            visited.remove(current)
+            return result
+
+        # Building the graph
+        graph = defaultdict(lambda: defaultdict(float))
+        for (dividend, divisor), value in zip(equations, values):
+            graph[dividend][divisor] = value
+            graph[divisor][dividend] = 1 / value
+        
+        # Processing queries
+        responses = []
+        for dividend, divisor in queries:
+            if dividend not in graph or divisor not in graph:
+                responses.append(-1.0)  # Either variable not in graph
+            elif dividend == divisor:
+                responses.append(1.0)   # Dividing something by itself
             else:
-                b_x,w_x=find(n)
-                b_y,w_y=find(d)
-                if b_x!=b_y:
-                    out.append(-1)
-                else:
-                    out.append(w_x/w_y)
-        return out
+                visited = set()
+                responses.append(backtrackEvaluate(dividend, divisor, visited, 1))
+        return responses
