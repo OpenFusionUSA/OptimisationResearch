9781fd29cd9c20c010eaa25792ead8710e93e67d
OpenFusionUSA <web.developer@openfusion.us>
2024-06-01 15:31:07 +0000

    Sync LeetCode submission - Word Break (python3)

--- my-folder/problems/word_break/solution.py
+++ my-folder/problems/word_break/solution.py
@@ -1,11 +1,18 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        @cache
-        def dp(i):
-            if i<0:
+        dp = [False] * (len(s) + 1)
+        dp[0] = True  # Base case: empty string can always be segmented
+
+        def check(i):
+            if dp[i]:
                 return True
             for word in wordDict:
-                if s[i-len(word)+1:i+1]==word and dp(i-len(word)):
+                if i >= len(word) and dp[i - len(word)] and s[i - len(word):i] == word:
+                    dp[i] = True
                     return True
             return False
-        return dp(len(s)-1)
+
+        for i in range(1, len(s) + 1):
+            check(i)
+
+        return dp[len(s)]
