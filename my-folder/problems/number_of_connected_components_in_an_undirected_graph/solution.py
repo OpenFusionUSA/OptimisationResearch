a0ef44b80b2a006ac543682169a966debe7c6261
OpenFusionUSA <web.developer@openfusion.us>
2024-06-03 00:41:42 +0000

    Sync LeetCode submission - Number of Connected Components in an Undirected Graph (python3)

--- my-folder/problems/number_of_connected_components_in_an_undirected_graph/solution.py
+++ my-folder/problems/number_of_connected_components_in_an_undirected_graph/solution.py
@@ -4,17 +4,17 @@ class Solution:
         for e in edges:
             adj[e[0]].append(e[1])
             adj[e[1]].append(e[0])
-        vis = set()
-        count = 0
+        vis=set()
+        count=0
         for i in range(n):
             if i in vis: continue
-            count += 1
-            st = [i]
+            count+=1
             vis.add(i)
-            while st:
-                curnode = st.pop()
-                for neigh in adj[curnode]:
-                    if neigh not in vis:
-                        vis.add(neigh)
-                        st.append(neigh)
+            q=[i]
+            while q:
+                node=q.pop()
+                for adjnodes in adj[node]:
+                    if adjnodes not in vis:
+                        vis.add(adjnodes)
+                        q.append(adjnodes)
         return count
