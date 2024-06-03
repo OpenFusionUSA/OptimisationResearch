6c0e3af6d6570a89f68c7c966c9c3469e77ed2b5
OpenFusionUSA <web.developer@openfusion.us>
2024-06-03 14:38:10 +0000

    Sync LeetCode submission - Kth Smallest Element in a BST (python3)

--- my-folder/problems/kth_smallest_element_in_a_bst/solution.py
+++ my-folder/problems/kth_smallest_element_in_a_bst/solution.py
@@ -6,22 +6,13 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        """
-        :type root: TreeNode
-        :type k: int
-        :rtype: int
-        """
-        def popksmall(root,k,ksmall):
-            if root==None: return 0
-            num = 0
-            if k>0:
-                num += popksmall(root.left,k,ksmall)
-            if num < k:
-                ksmall.append(root.val)
-                num += 1
-            if num < k:
-                num += popksmall(root.right, k-num, ksmall)
-            return num
-        ksmall = []
-        popksmall(root,k,ksmall)
-        return ksmall[-1]
+        stack=[]
+        while True:
+            while root:
+                stack.append(root)
+                root=root.left
+            root=stack.pop()
+            k-=1
+            if k==0:
+                return root.val
+            root=root.right
