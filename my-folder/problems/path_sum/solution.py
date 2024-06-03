31582e437618b31fd1f5cf782a30f186dbd5f18f
OpenFusionUSA <web.developer@openfusion.us>
2024-06-03 15:47:54 +0000

    Sync LeetCode submission - Path Sum (python3)

--- my-folder/problems/path_sum/solution.py
+++ my-folder/problems/path_sum/solution.py
@@ -6,9 +6,10 @@
 #         self.right = right
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        if not root:
-            return False
-        targetSum-=root.val
-        if not root.left and not root.right:
-            return targetSum==0
-        return self.hasPathSum(root.left,targetSum) or self.hasPathSum(root.right,targetSum)
+        def dfs(root,currentsum):
+            if not root:return False
+            currentsum=currentsum+root.val
+            if not root.left and not root.right:
+                return currentsum==targetSum
+            return dfs(root.left,currentsum) or dfs(root.right, currentsum)
+        return dfs(root, 0)
