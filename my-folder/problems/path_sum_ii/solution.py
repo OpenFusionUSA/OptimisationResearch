dd072b199943a6ad32f4ac15e14a4302d8509c45
OpenFusionUSA <web.developer@openfusion.us>
2024-06-03 15:57:47 +0000

    Sync LeetCode submission - Path Sum II (python3)

--- my-folder/problems/path_sum_ii/solution.py
+++ my-folder/problems/path_sum_ii/solution.py
@@ -1,15 +1,26 @@
-class Solution(object):
-    def recursive(self,node,remainingSum,pathNode,pathList):
-        if not node:
-            return 
-        pathNode.append(node.val)
-        if node.val==remainingSum and not node.left and not node.right:
-            pathList.append(list(pathNode))
-        else:
-            self.recursive(node.left,remainingSum - node.val, pathNode,pathList)
-            self.recursive(node.right,remainingSum - node.val, pathNode,pathList)
-        pathNode.pop()
-    def pathSum(self, root, targetSum):
-        pathList=[]
-        self.recursive(root,targetSum,[],pathList)
-        return pathList
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        def dfs(node,results,currentpath,currentSum):
+            if not node:
+                return
+            currentpath.append(node.val)
+            currentSum+=node.val
+            if not node.left and not node.right:
+                if currentSum==targetSum:
+                    results.append(list(currentpath))
+            else:
+                dfs(node.left,results,currentpath,currentSum)
+                dfs(node.right,results,currentpath,currentSum)
+            currentpath.pop()
+            currentSum-=node.val
+
+        results=[]
+        currentpath=[]
+        dfs(root, results, currentpath, 0)
+        return results
