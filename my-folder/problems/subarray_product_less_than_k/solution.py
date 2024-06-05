6c49a1fc4c2a5661f1815c4a2ae5788614cbfbd6
OpenFusionUSA <web.developer@openfusion.us>
2024-06-05 00:04:55 +0000

    Sync LeetCode submission - Subarray Product Less Than K (python3)

--- my-folder/problems/subarray_product_less_than_k/solution.py
+++ my-folder/problems/subarray_product_less_than_k/solution.py
@@ -1,14 +1,11 @@
 class Solution:
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
-        l=0
-        if k <= 1:
-            return 0
-        n=len(nums)
-        product=1
         count=0
-        for r,num in enumerate(nums):
-            product*=num
-            while product>=k:
+        l=0
+        product=1
+        for r in range(len(nums)):
+            product*=nums[r] 
+            while product>=k and l<=r:
                 product=product/nums[l]
                 l+=1
             count+=r-l+1
