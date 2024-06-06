ba939b1d8198ca87dd4f23ca3ec6c8309ebf7788
OpenFusionUSA <web.developer@openfusion.us>
2024-06-06 01:55:09 +0000

    Sync LeetCode submission - Merge k Sorted Lists (python3)

--- my-folder/problems/merge_k_sorted_lists/solution.py
+++ my-folder/problems/merge_k_sorted_lists/solution.py
@@ -3,21 +3,19 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-import heapq
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         n=len(lists)
-        dummy = ListNode(None)
-        current = dummy
+        dummy=ListNode(None)
+        current=dummy
         pq=[]
         for i,node in enumerate(lists):
             if node:
-                heapq.heappush(pq,(node.val,i,node))
+                heapq.heappush(pq, (node.val,i,node))
         while pq:
-            min_element,index,node=heapq.heappop(pq)
+            _,index,node=heapq.heappop(pq)
             current.next=node
-            current=current.next
+            current = current.next
             if node.next:
-                heapq.heappush(pq,(node.next.val,index,node.next))
+                heapq.heappush(pq, (node.next.val,index,node.next))
         return dummy.next
-            
