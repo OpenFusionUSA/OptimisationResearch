236477f14474d46cfcd2d3c79dda3afefc16a6ab
OpenFusionUSA <web.developer@openfusion.us>
2024-06-06 02:38:33 +0000

    Sync LeetCode submission - Symmetric Tree (python)

--- my-folder/problems/symmetric_tree/solution.py
+++ my-folder/problems/symmetric_tree/solution.py
@@ -5,20 +5,15 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
-    def isMirror(self,left,right):
-        if left is None and right is None:
-            return True
-        if left is None and right is not None:
-            return False
-        if left is not None and right is None:
-            return False
-        if left.val==right.val and self.isMirror(left.left,right.right) and self.isMirror(left.right,right.left):
-            return True
     def isSymmetric(self, root):
         """
         :type root: TreeNode
         :rtype: bool
         """
-        if not root:
-            return True
-        return self.isMirror(root.left, root.right)
+        def isMirror(t1,t2):
+            if t1 is None and t2 is None:
+                return True
+            if t1 is None or t2 is None:
+                return False
+            return (t1.val==t2.val) and isMirror(t1.left,t2.right) and isMirror(t1.right,t2.left)
+        return isMirror(root.left,root.right)
