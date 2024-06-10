2d869781253ffae8253d0772762b54c810846722
OpenFusionUSA <web.developer@openfusion.us>
2024-06-10 19:53:55 +0000

    Sync LeetCode submission - Meeting Rooms II (python3)

--- my-folder/problems/meeting_rooms_ii/solution.py
+++ my-folder/problems/meeting_rooms_ii/solution.py
@@ -2,11 +2,10 @@ class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         if not intervals:
             return 0
-        intervals.sort(key=lambda x:x[0])
-        free_rooms=[]
-        heapq.heappush(free_rooms, intervals[0][1])
-        for [s,d] in intervals[1:]:
-            if free_rooms[0]<=s:
-                heapq.heappop(free_rooms)
-            heapq.heappush(free_rooms, d)
-        return len(free_rooms)
+        intervals=sorted(intervals, key=lambda x:x[0])
+        minheap=[0]
+        for i in intervals:
+            if i[0]>=minheap[0]:
+                heapq.heappop(minheap)
+            heapq.heappush(minheap, i[1])
+        return len(minheap)
