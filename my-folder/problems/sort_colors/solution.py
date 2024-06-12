0080612fdc6cb786672b65f3a686978e3ae69f6d
OpenFusionUSA <web.developer@openfusion.us>
2024-06-12 02:10:02 +0000

    Sync LeetCode submission - Sort Colors (python3)

--- my-folder/problems/sort_colors/solution.py
+++ my-folder/problems/sort_colors/solution.py
@@ -1,18 +1,11 @@
-class Solution(object):
-    def sortColors(self, nums):
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
         """
-        :type nums: List[int]
-        :rtype: None Do not return anything, modify nums in-place instead.
+        Do not return anything, modify nums in-place instead.
         """
-        low=mid=0
-        high=len(nums)-1
-        while mid<=high:
-            if nums[mid]==0:
-                nums[low],nums[mid]=nums[mid],nums[low]
-                low+=1
-                mid+=1
-            elif nums[mid]==2:
-                nums[high],nums[mid]=nums[mid],nums[high]
-                high-=1
-            else:
-                mid+=1                
+        n=len(nums)
+        for i in range(n):
+            for j in range(n-i-1):
+                if nums[j]>nums[j+1]:
+                    nums[j],nums[j+1]=nums[j+1],nums[j]
+        return nums
