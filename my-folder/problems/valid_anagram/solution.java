71d0892842da53c87a91abc1af7cd8df462fa6d5
OpenFusionUSA <web.developer@openfusion.us>
2024-07-02 23:16:56 +0000

    Sync LeetCode submission - Valid Anagram (java)

--- my-folder/problems/valid_anagram/solution.java
+++ my-folder/problems/valid_anagram/solution.java
@@ -1,18 +1,20 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-    if (s.length() != t.length()) {
-        return false;
-    }
-    int[] counter = new int[26];
-    for (int i = 0; i < s.length(); i++) {
-        counter[s.charAt(i) - 'a']++;
-        counter[t.charAt(i) - 'a']--;
-    }
-    for (int count : counter) {
-        if (count != 0) {
+        int n=s.length();
+        int m=t.length();
+        if (n!=m){
             return false;
         }
-    }
-    return true;
-}
+        int[] counter = new int[26];
+        for ( int i=0; i<n; i++){
+            counter[s.charAt(i)-'a']++;
+            counter[t.charAt(i)-'a']--;
+            }
+        for ( int count: counter){
+            if (count!=0){
+                return false;
+            }
+        }
+        return true;
+        }
 }
