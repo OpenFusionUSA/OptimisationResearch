7261362514eb24e8bbec28c04977011072a85e2c
OpenFusionUSA <web.developer@openfusion.us>
2024-07-02 16:31:26 +0000

    Sync LeetCode submission - Intersection of Two Arrays II (python3)

---
+++ my-folder/problems/intersection_of_two_arrays_ii/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1.sort()
+        nums2.sort()
+        i=0
+        j=0
+        k=0
+        while i<len(nums1) and j<len(nums2):
+            if nums1[i]<nums2[j]:
+                i+=1
+            elif nums1[i]>nums2[j]:
+                j+=1
+            else:
+                nums1[k]=nums1[i]
+                k+=1
+                i+=1
+                j+=1
+        return nums1[0:k]
+
