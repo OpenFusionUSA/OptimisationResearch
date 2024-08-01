da0266dbb4b92a8e29d49dfac895d41c874ce3ae
OpenFusionUSA <web.developer@openfusion.us>
2024-08-01 19:51:56 +0000

    Sync LeetCode submission - Coin Change (python3)

--- my-folder/problems/coin_change/solution.py
+++ my-folder/problems/coin_change/solution.py
@@ -1,9 +1,10 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         coins.sort()
-        dp=[float('inf')]*(amount+1)
+        dp=[math.inf]*(amount+1)
+        ### selecting nothing is one way of getting 0 sum
         dp[0]=0
         for c in coins:
-            for x in range(c,amount+1):
-                dp[x]=min(dp[x],dp[x-c]+1)
-        return dp[amount] if dp[amount]!=float('inf') else -1
+            for i in range(c,amount+1):
+                dp[i]=min(dp[i],dp[i-c]+1)
+        return dp[amount] if dp[amount]!=math.inf else -1
