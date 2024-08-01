14383460e11ddda1d584b8247ebd56aeb8fdff77
OpenFusionUSA <web.developer@openfusion.us>
2024-08-01 01:23:13 +0000

    Sync LeetCode submission - Rotate Image (python3)

--- my-folder/problems/rotate_image/solution.py
+++ my-folder/problems/rotate_image/solution.py
@@ -1,15 +1,17 @@
-class Solution(object):
-    def rotate(self, matrix):
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
         """
-        :type matrix: List[List[int]]
-        :rtype: None Do not return anything, modify matrix in-place instead.
+        Do not return anything, modify matrix in-place instead.
         """
-        n=len(matrix[0])
-        for i in range(n//2+n%2):
+        self.transpose(matrix)
+        self.r(matrix)
+    def transpose(self, matrix: List[List[int]]) -> None:
+        n=len(matrix)
+        for i in range(n):
+            for j in range(i+1,n):
+                matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
+    def r(self, matrix: List[List[int]]) -> None:
+        n=len(matrix)
+        for i in range(n):
             for j in range(n//2):
-                temp=matrix[n-1-j][i]
-                matrix[n-1-j][i]=matrix[n-1-i][n-1-j]
-                matrix[n-1-i][n-1-j]=matrix[j][n-1-i]
-                matrix[j][n-1-i]=matrix[i][j]
-                matrix[i][j]=temp
-        
+                matrix[i][j],matrix[i][n-j-1]=matrix[i][n-j-1],matrix[i][j]
