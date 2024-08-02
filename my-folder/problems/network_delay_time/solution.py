025d595282af4b23ac10a416770be8b28cc5dee7
OpenFusionUSA <web.developer@openfusion.us>
2024-08-02 17:27:33 +0000

    Sync LeetCode submission - Network Delay Time (python3)

--- my-folder/problems/network_delay_time/solution.py
+++ my-folder/problems/network_delay_time/solution.py
@@ -1,16 +1,16 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         graph=defaultdict(list)
-        for src,dest,weight in times:
-            graph[src].append([dest,weight])
-        minheap=[[0,k]]
+        for [s,t,d] in times:
+            graph[s].append([t,d])
+        minheap=[(0,k)]
         mindist={}
         while minheap:
             distance,node=heapq.heappop(minheap)
             if node in mindist:
                 continue
             mindist[node]=distance
-            for adjnode,adjdist in graph[node]:
-                if adjnode not in mindist:
-                    heapq.heappush(minheap, ((adjdist+distance,adjnode)))
+            for adjnodes,adjdist in graph[node]:
+                if adjnodes not in mindist:
+                    heapq.heappush(minheap, (distance+adjdist,adjnodes))
         return max(mindist.values()) if len(mindist)==n else -1
