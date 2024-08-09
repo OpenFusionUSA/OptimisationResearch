a1b189ccdd9347075635e7d42f0f7d397130bd99
OpenFusionUSA <web.developer@openfusion.us>
2024-08-09 00:28:39 +0000

    Sync LeetCode submission - Sliding Window Maximum (java)

--- my-folder/problems/sliding_window_maximum/solution.java
+++ my-folder/problems/sliding_window_maximum/solution.java
@@ -1,19 +1,19 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        Deque<Integer> deque = new LinkedList<>();
-        List<Integer> resp= new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            while (!deque.isEmpty() && deque.peekFirst()<(i-k+1)){
-                deque.pollFirst();
-            }
-            while (!deque.isEmpty() && nums[deque.peekLast()]<nums[i]){
-                deque.pollLast();
-            }
-            deque.addLast(i);
-            if (i>=k-1){
-                resp.add(nums[deque.peekFirst()]);
-            }
+     Deque<Integer> dq = new LinkedList<>();
+     List<Integer> resp = new ArrayList();
+     for(int i=0; i< nums.length; i++){
+        while (!dq.isEmpty() && dq.peekFirst()<(i-k+1)){
+            dq.pollFirst();
         }
-        return resp.stream().mapToInt(Integer::intValue).toArray();
+        while (!dq.isEmpty() && nums[dq.peekLast()] < nums[i]){
+            dq.pollLast();
+        }
+        dq.addLast(i);
+        if(i>=k-1){
+            resp.add(nums[dq.peekFirst()]);
+        }
+     }
+     return resp.stream().mapToInt(Integer::intValue).toArray();   
     }
 }
