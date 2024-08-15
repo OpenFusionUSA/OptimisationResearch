eb971027b15ac51fdd6c65f6cf2cff8a12e61271
OpenFusionUSA <web.developer@openfusion.us>
2024-08-15 23:54:26 +0000

    Sync LeetCode submission - Web Crawler Multithreaded (java)

--- my-folder/problems/web_crawler_multithreaded/solution.java
+++ my-folder/problems/web_crawler_multithreaded/solution.java
@@ -7,52 +7,70 @@
  */
 class Solution {
     public List<String> crawl(String startUrl, HtmlParser htmlParser) {
-       String hostname= getHostName(startUrl);
-       List<String> res = new ArrayList<>();
-       Set<String> visited = new HashSet<>();
-       BlockingQueue<String> q = new LinkedBlockingQueue<>();
-       Deque<Future> dq = new ArrayDeque<>();
-       q.offer(startUrl);
-       ExecutorService executor = Executors.newFixedThreadPool(4, r->{
-        Thread t = new Thread(r);
-        t.setDaemon(true);
-        return t;
-       });
 
-       while(true){
-            String url = q.poll();
-            if (url!=null){
-                if (getHostName(url).equals(hostname) && !visited.contains(url)){
-                    res.add(url);
-                    visited.add(url);
-                    dq.offerFirst(executor.submit(()->{
-                        List<String> newUrls = htmlParser.getUrls(url);
-                        for (String u :newUrls){
-                            q.offer(u);
-                        }
-                    }));
-                }
-            }
-            else{
-                if (dq.isEmpty()){
-                    break;
-                }
-                Future task = dq.poll();
-                try{
-                    task.get();
-                }
-                catch (InterruptedException | ExecutionException e) {}
-            }
-       }
-       return res;
+        String hostname = getHostname(startUrl);
+        Set<String> set = ConcurrentHashMap.newKeySet();
+        set.add(startUrl);
+        
+        ExecutorService executor = Executors.newCachedThreadPool();
+        
+        UrlCallable task = new UrlCallable(startUrl, htmlParser, set, hostname, executor);
+        Future future = executor.submit(task);
+        
+        try {
+            future.get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+        
+        executor.shutdown();
+        return new ArrayList(set);
     }
-
-    public String getHostName(String url){
-        url=url.substring(7);
-        String[] parts = url.split("/");
-        return parts[0];
-    }
-
-
     
+    private String getHostname(String url) {
+        int idx = url.indexOf('/', 7);
+        return (idx != -1) ? url.substring(0, idx) : url;
+    }
+}
+
+class UrlCallable implements Callable<Void>{
+    private String inputUrl;
+    private HtmlParser htmlParser;
+    private Set<String> set;
+    private String hostName;
+    private ExecutorService executorService;
+
+        public UrlCallable(String inputUrl, HtmlParser htmlParser, Set<String> set, String hostName, ExecutorService executorService){
+        this.inputUrl=inputUrl;
+        this.htmlParser=htmlParser;
+        this.set=set;
+        this.hostName=hostName;
+        this.executorService=executorService;
+    }
+
+    public boolean isSameHost(String url, String hostName){
+        if(!url.startsWith(hostName)){
+            return false;
+        }
+        return url.length()==hostName.length() || url.charAt(hostName.length())=='/';
+    }
+
+    @Override
+    public Void call(){
+        List<Future> futures = new ArrayList<>();
+        htmlParser.getUrls(inputUrl).parallelStream().filter(url-> isSameHost(url,hostName)).filter(url->set.add(url))
+        .forEach(url-> {
+            UrlCallable task = new UrlCallable(url,htmlParser,set,hostName,executorService);
+            Future future = executorService.submit(task);
+            futures.add(future);
+        });
+        futures.forEach(future -> {
+            try{ future.get();
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
+        });
+        return null;
+    }
 }
