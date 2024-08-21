5c77f473aefe2e8954717e427680f04c26298533
OpenFusionUSA <web.developer@openfusion.us>
2024-08-21 19:52:41 +0000

    Sync LeetCode submission - Merge Intervals (python3)

--- my-folder/problems/merge_intervals/solution.py
+++ my-folder/problems/merge_intervals/solution.py
@@ -1,10 +1,10 @@
 class Solution:
-    def merge (self, intervals: List[List[int]]) -> List[List[int]]:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort(key=lambda x:x[0])
         merged=[]
-        for inte in intervals:
-            if not merged or merged[-1][1]<inte[0]:
-                merged.append(inte)
+        for interval in intervals:
+            if not merged or merged[-1][1]<interval[0]:
+                merged.append(interval)
             else:
-                merged[-1][1]=max(inte[1],merged[-1][1])
+                merged[-1][1]=max(merged[-1][1],interval[1])
         return merged
