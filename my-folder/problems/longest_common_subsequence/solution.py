e6deafb1b2d64d0793bb1db245813eb48dc61a50
OpenFusionUSA <web.developer@openfusion.us>
2024-08-22 21:32:04 +0000

    Sync LeetCode submission - Longest Common Subsequence (python3)

--- my-folder/problems/longest_common_subsequence/solution.py
+++ my-folder/problems/longest_common_subsequence/solution.py
@@ -5,9 +5,7 @@ class Solution:
         dp=[[0 for _ in range(n+1)] for _ in range(m+1)]
         for i in range(1,m+1):
             for j in range(1,n+1):
-                if i==0 and j==0 and text1[i-1]==text2[j-1]:
-                    dp[i][j]=0
-                elif text1[i-1]==text2[j-1]:
+                if text1[i-1]==text2[j-1]:
                     dp[i][j]=dp[i-1][j-1]+1
                 else:
                     dp[i][j]=max(dp[i][j-1],dp[i-1][j])
