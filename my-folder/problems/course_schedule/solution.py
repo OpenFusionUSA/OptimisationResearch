c1416c87fee8468ca8af9fc6aaaecf98a65e5de0
OpenFusionUSA <web.developer@openfusion.us>
2024-08-22 18:07:43 +0000

    Sync LeetCode submission - Course Schedule (python3)

--- my-folder/problems/course_schedule/solution.py
+++ my-folder/problems/course_schedule/solution.py
@@ -1,20 +1,20 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        adj={i:[] for i in range(numCourses)}
         indegree=[0]*numCourses
-        for a,b in prerequisites:
-            adj[b].append(a)
-            indegree[a]+=1
+        graph=defaultdict(list)
+        for prereq in prerequisites:
+            graph[prereq[1]].append(prereq[0])
+            indegree[prereq[0]]+=1
         q=deque()
         for i in range(numCourses):
             if indegree[i]==0:
                 q.append(i)
-        noofcourses=0
+        completed=0
         while q:
             node=q.pop()
-            noofcourses+=1
-            for c in adj[node]:
-                indegree[c]-=1
-                if indegree[c]==0:
-                    q.append(c)
-        return noofcourses==numCourses
+            completed+=1
+            for adjnode in graph[node]:
+                indegree[adjnode]-=1
+                if indegree[adjnode]==0:
+                    q.append(adjnode)
+        return completed==numCourses
