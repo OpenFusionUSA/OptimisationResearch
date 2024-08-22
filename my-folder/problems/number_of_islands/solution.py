180d026d8f5a00760834a9c29c2ec8eda5d5ef40
OpenFusionUSA <web.developer@openfusion.us>
2024-08-22 01:38:25 +0000

    Sync LeetCode submission - Number of Islands (python3)

--- my-folder/problems/number_of_islands/solution.py
+++ my-folder/problems/number_of_islands/solution.py
@@ -1,28 +1,28 @@
 class Solution:
-    def dfs(self,q,grid):
-        dir=[(1,0),(-1,0),(0,1),(0,-1)]
-        visited=set()
-        m=len(grid)
-        n=len(grid[0])
-        while q:
-            (x,y)=q.pop()
-            visited.add((x,y))
-            for (i,j) in dir:
-                mx,my=x+i,y+j
-                if (mx,my) in visited:
-                    continue
-                if 0<=mx<m and 0<=my<n and grid[mx][my]=="1":
-                    q.append((mx,my))
-                    grid[mx][my]=0
     def numIslands(self, grid: List[List[str]]) -> int:
-        islands=0
+        ans=0
         m=len(grid)
         n=len(grid[0])
         for i in range(m):
             for j in range(n):
                 if grid[i][j]=="1":
                     q=deque()
-                    islands+=1
                     q.append((i,j))
-                    self.dfs(q,grid)
-        return islands
+                    ans+=1
+                    self.dfs(q, grid)
+        return ans
+    def dfs(self,q,grid):
+        visited=set()
+        dir=[(0,1),(0,-1),(1,0),(-1,0)]
+        m=len(grid)
+        n=len(grid[0])
+        while q:
+            (x,y)=q.pop()
+            for (dx,dy) in dir:
+                nx,ny=x+dx,y+dy
+                if (nx,ny) in visited:
+                    continue
+                if 0<=nx<m and 0<=ny<n and grid[nx][ny]=="1":
+                    q.append((nx,ny))
+                    grid[nx][ny]="0"
+        
