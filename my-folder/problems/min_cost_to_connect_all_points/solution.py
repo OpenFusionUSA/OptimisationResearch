280151563db016c823117dc63bfb556515fc4ede
OpenFusionUSA <web.developer@openfusion.us>
2024-08-23 00:47:31 +0000

    Sync LeetCode submission - Min Cost to Connect All Points (python3)

--- my-folder/problems/min_cost_to_connect_all_points/solution.py
+++ my-folder/problems/min_cost_to_connect_all_points/solution.py
@@ -1,29 +1,25 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        def nested_dict():
-            return defaultdict(nested_dict)
-        graph=defaultdict(nested_dict)
+        ### prims 
+        graph=defaultdict(defaultdict)
         m=len(points)
-        n=len(points[0])
         for i in range(m):
-            for j in range(m):
-                if i!=j:
-                    dist=abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1])
-                    graph[i][j]=dist
-                    graph[j][i]=dist
+            for j in range(i,m):
+                dist=abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1])
+                graph[i][j]=dist
+                graph[j][i]=dist
         minheap=[]
-        heapq.heappush(minheap, (0,0))
-        mincost=0
+        heapq.heappush(minheap,(0,0))
         visited=set()
+        mincost=0
         while minheap:
             distance,node=heapq.heappop(minheap)
             if node not in visited:
                 mincost+=distance
                 visited.add(node)
-                for adjnode,adjdist in graph[node].items():
-                    heapq.heappush(minheap, (adjdist,adjnode))
-        if len(visited)==m: 
+                for adjnode,distance in graph[node].items():
+                    heapq.heappush(minheap, (distance,adjnode))
+        if len(visited)==m:
             return mincost
         else:
             return -1
-
