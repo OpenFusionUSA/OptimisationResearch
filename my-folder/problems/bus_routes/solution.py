6ca0437344f5ea1a42d9453df2f42b9f3abfa994
OpenFusionUSA <web.developer@openfusion.us>
2024-09-02 23:20:19 +0000

    Sync LeetCode submission - Bus Routes (python3)

--- my-folder/problems/bus_routes/solution.py
+++ my-folder/problems/bus_routes/solution.py
@@ -1,29 +1,31 @@
-from collections import defaultdict
+from collections import defaultdict, deque
 
-class Solution:
-    def numBusesToDestination(self, routes: list[list[int]], source: int, target: int) -> int:
+class Solution(object):
+    def numBusesToDestination(self, routes, source, target):
+        # If the source is the same as the target, no travel is needed.
         if source == target:
             return 0
 
-        graph = defaultdict(set)
+        # routes = [set(route) for route in routes]  # Convert each route to a set for O(1) lookups
+        stop_to_routes = defaultdict(set)  # Use set to prevent duplicates
         for i, route in enumerate(routes):
             for stop in route:
-                graph[stop].add(i)
+                stop_to_routes[stop].add(i)
 
-        visited_stops = set()
-        visited_routes = set()
-        visited_stops.add(source)
-        queue = [(source, 0)]
+        visited_routes = set()  # To keep track of visited routes
+        visited_stops = {source}  # To keep track of visited stops
+        queue = deque([(source, 0)])  # Queue now only needs the current stop and the bus count
 
         while queue:
-            current_stop, bus_count = queue.pop(0)
-            for route_i in graph[current_stop]:
-                if route_i not in visited_routes:
+            current_stop, bus_count = queue.popleft()
+            for route_i in stop_to_routes[current_stop]:
+                if route_i not in visited_routes:  # Check the route only if not visited
                     visited_routes.add(route_i)
-                    for stop in routes[route_i]:
-                        if stop == target:
+                    for next_stop in routes[route_i]:
+                        if next_stop == target:  # Found the target
                             return bus_count + 1
-                        if stop not in visited_stops:
-                            visited_stops.add(stop)
-                            queue.append((stop, bus_count + 1))
-        return -1
+                        if next_stop not in visited_stops:  # If stop is not visited, add to queue
+                            visited_stops.add(next_stop)
+                            queue.append((next_stop, bus_count + 1))
+
+        return -1  # If the target is unreachable
