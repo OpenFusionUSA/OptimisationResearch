6fcbd502635b89da9e0570880b7ab6a786e24f64
OpenFusionUSA <web.developer@openfusion.us>
2024-09-04 02:06:01 +0000

    Sync LeetCode submission - Letter Combinations of a Phone Number (python3)

--- my-folder/problems/letter_combinations_of_a_phone_number/solution.py
+++ my-folder/problems/letter_combinations_of_a_phone_number/solution.py
@@ -1,13 +1,25 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        ans=[]
-        letters={"2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
-        def recur(index,str):
-            if len(str)==len(digits):
-                ans.append(str)
+        out=[]
+        letters = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+        def backtrack(index,currpath):
+            if index==len(digits):
+                out.append("".join(currpath))
                 return
-            for c in letters[digits[index]]:
-                recur(index+1,str+c)
-        if digits:
-            recur(0,"")
-        return ans
+            for letter in letters[digits[index]]:
+                currpath.append(letter)
+                backtrack(index+1, currpath)
+                currpath.pop()
+        backtrack(0, [])
+        if len(digits)==0:
+            return []
+        return out
