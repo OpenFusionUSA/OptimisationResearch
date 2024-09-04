27454b51b2a33b7146c75e22d143d63a31393598
OpenFusionUSA <web.developer@openfusion.us>
2024-09-04 00:51:48 +0000

    Sync LeetCode submission - Longest Consecutive Sequence (python3)

--- my-folder/problems/longest_consecutive_sequence/solution.py
+++ my-folder/problems/longest_consecutive_sequence/solution.py
@@ -1,17 +1,11 @@
-class Solution(object):
-    def longestConsecutive(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        longest=0
-        num_set=set(nums)
-        for num in num_set:
-            if num-1 not in num_set:
-                current_num=num
-                current_streak=1
-                while current_num +1 in num_set:
-                    current_num+=1
-                    current_streak+=1
-                longest=max(longest,current_streak)
-        return longest
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        s = set(nums)
+        ans = 0
+        for i in s:
+            if i-1 not in s:
+                y = i+1
+                while y in s:
+                    y += 1
+                ans = max(ans, y-i)
+        return ans
