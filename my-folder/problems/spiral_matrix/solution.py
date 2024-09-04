642735a439387071559e5d0694fa74e5a88e3280
OpenFusionUSA <web.developer@openfusion.us>
2024-09-04 00:28:11 +0000

    Sync LeetCode submission - Spiral Matrix (python3)

--- my-folder/problems/spiral_matrix/solution.py
+++ my-folder/problems/spiral_matrix/solution.py
@@ -1,24 +1,19 @@
-class Solution(object):
-    def spiralOrder(self, matrix):
-        """
-        :type matrix: List[List[int]]
-        :rtype: List[int]
-        """
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         m=len(matrix)
         n=len(matrix[0])
         out=[]
-        up=left=0
-        right=n-1
-        bottom=m-1
-        while len(out)< (m)*(n) :
-            for col in range(left, right+1):
+        up,left=0,0
+        bottom,right=m-1,n-1
+        while len(out)<(m*n):
+            for col in range(left,right+1):
                 out.append(matrix[up][col])
             for row in range(up+1,bottom+1):
                 out.append(matrix[row][right])
             if up<bottom:
                 for col in range(right-1,left-1,-1):
                     out.append(matrix[bottom][col])
-                if left < right :
+                if left<right:
                     for row in range(bottom-1,up,-1):
                         out.append(matrix[row][left])
             left+=1
