8ed681d3a0d1cff814c9ac933dd4dce83d57427e
OpenFusionUSA <web.developer@openfusion.us>
2024-09-08 17:13:23 +0000

    Sync LeetCode submission - Insert Interval (python3)

--- my-folder/problems/insert_interval/solution.py
+++ my-folder/problems/insert_interval/solution.py
@@ -1,18 +1,17 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         result=[]
-        inserted=False
+        added=False
         for s,e in intervals:
             if e<newInterval[0]:
                 result.append([s,e])
             elif s>newInterval[1]:
-                if not inserted:
+                if not added:
                     result.append(newInterval)
-                    inserted=True
+                    added=True
                 result.append([s,e])
             else:
-                newInterval[0]=min(newInterval[0],s)
-                newInterval[1]=max(newInterval[1], e)
-        if not result or not inserted:
+                newInterval=[min(newInterval[0], s),max(newInterval[1], e)]
+        if not added:
             result.append(newInterval)
         return result
