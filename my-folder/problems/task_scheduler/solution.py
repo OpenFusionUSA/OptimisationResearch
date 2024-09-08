aa22362beab651826ead1d20b6deadfba9b56906
OpenFusionUSA <web.developer@openfusion.us>
2024-09-08 03:01:34 +0000

    Sync LeetCode submission - Task Scheduler (python3)

--- my-folder/problems/task_scheduler/solution.py
+++ my-folder/problems/task_scheduler/solution.py
@@ -1,22 +1,6 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        c=[0]*26
-        for task in tasks:
-            c[ord(task)-ord('A')]+=1
-        pq=[-f for f in c if f>0]
-        heapq.heapify(pq) 
-        time=0
-        while pq:
-            cycle=n+1
-            store=[]
-            t=0
-            while pq and cycle>0:
-                c=-heapq.heappop(pq)
-                if c>1:
-                    store.append(-(c-1))
-                t+=1
-                cycle-=1
-            for x in store:
-                heapq.heappush(pq,x)
-            time+=t if not pq else n+1
-        return time
+        freq=Counter(tasks)
+        maxf=max(freq.values())
+        s=sum([maxf==f for k,f in freq.items()])
+        return max(len(tasks), (n+1)*(maxf-1)+s)
