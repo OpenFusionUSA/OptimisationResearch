b86adff16b327777bd8a2445be2bb9a87b81b106
OpenFusionUSA <web.developer@openfusion.us>
2024-09-10 15:48:51 +0000

    Sync LeetCode submission - Maximum Subarray (python3)

--- my-folder/problems/maximum_subarray/solution.py
+++ my-folder/problems/maximum_subarray/solution.py
@@ -1,8 +1,7 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        n=len(nums)
-        dp=[0]*n
+        dp=[-math.inf]*len(nums)
         dp[0]=nums[0]
-        for i in range(1,n):
+        for i in range(1,len(nums)):
             dp[i]=max(dp[i-1]+nums[i],nums[i])
         return max(dp)
