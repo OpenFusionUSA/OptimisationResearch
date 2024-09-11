8e45020ce0400bb705e28202360442ae0cc774e7
OpenFusionUSA <web.developer@openfusion.us>
2024-09-11 19:33:52 +0000

    Sync LeetCode submission - Amount of Time for Binary Tree to Be Infected (python3)

--- my-folder/problems/amount_of_time_for_binary_tree_to_be_infected/solution.py
+++ my-folder/problems/amount_of_time_for_binary_tree_to_be_infected/solution.py
@@ -1,39 +1,48 @@
+from collections import defaultdict, deque
+from typing import Optional
+
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
-    def convert(self,current: TreeNode, parent: int, tree_map: Dict[int, Set[int]]):
-        if current is None:
-            return
-        if current not in tree_map:
-            tree_map[current.val]=set()
-        adj_list=tree_map[current.val]
-        if parent!=0:
-            adj_list.add(parent)
-        if current.left:
-            adj_list.add(current.left.val)
-        if current.right:
-            adj_list.add(current.right.val)
-        self.convert(current.left,current.val,tree_map)
-        self.convert(current.right,current.val,tree_map)
-        
     def amountOfTime(self, root: Optional[TreeNode], start: int) -> int:
-        tree_map=defaultdict()
-        self.convert(root,0,tree_map)
-        queue=deque([start])
-        visited={start}
-        distance=0
-        while queue:
-            level_size=len(queue)
-            while level_size>0:
-                current=queue.popleft()
-                for num in tree_map[current]:
-                    if num not in visited:
-                        visited.add(num)
-                        queue.append(num)
-                level_size-=1
-            distance+=1
+        
+        # Function to construct the tree into an undirected graph
+        def constructTree(node, parent, graph):
+            if not node:
+                return
+            # Add the bidirectional edge between parent and child
+            if parent is not None:
+                graph[node.val].add(parent)
+                graph[parent].add(node.val)
+            # Recursive calls to build for left and right children
+            if node.left:
+                constructTree(node.left, node.val, graph)
+            if node.right:
+                constructTree(node.right, node.val, graph)
+
+        # Build the graph from the tree
+        graph = defaultdict(set)
+        constructTree(root, None, graph)
+        
+        # BFS to find the furthest distance from the start node
+        q = deque([start])
+        visited = set([start])
+        distance = 0
+        
+        while q:
+            n = len(q)
+            for _ in range(n):
+                current = q.popleft()
+                # Explore neighbors (adjacent nodes in the graph)
+                for neighbor in graph[current]:
+                    if neighbor not in visited:
+                        visited.add(neighbor)
+                        q.append(neighbor)
+            distance += 1
+        
         return distance-1
