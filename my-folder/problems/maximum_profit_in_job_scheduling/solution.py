9679eeb51f7164327ee9fc80dc870d142aa971a0
OpenFusionUSA <web.developer@openfusion.us>
2024-09-12 14:58:36 +0000

    Sync LeetCode submission - Maximum Profit in Job Scheduling (python3)

--- my-folder/problems/maximum_profit_in_job_scheduling/solution.py
+++ my-folder/problems/maximum_profit_in_job_scheduling/solution.py
@@ -1,36 +1,15 @@
-class Solution(object):
-    def jobScheduling(self, startTime, endTime, profit):
-        """
-        :type startTime: List[int]
-        :type endTime: List[int]
-        :type profit: List[int]
-        :rtype: int
-        """
-        n=len(startTime)
-        mem=[-1]*50000
-        jobs=[s for s in zip(startTime,endTime,profit)]
-        jobs.sort(key=lambda x:x[0])
-        startTime.sort()
-        def findNextJobIndex(startTime,lastjobendtime):
-            start=0
-            end=len(startTime)-1
-            ni=len(startTime)
-            while start<=end:
-                mid=(start+end)/2
-                if startTime[mid]>=lastjobendtime:
-                    ni=mid
-                    end=mid-1
-                else:
-                    start=mid+1
-            return ni
-        def findMaxProfit(jobs,startTime,i):
-            if i==n:
+class Solution:
+    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+        jobs=sorted(zip(startTime,endTime,profit))
+        n=len(jobs)
+        memo={}
+        def dfs(i):
+            if i in memo:
+                return memo[i]
+            if i>=n:
                 return 0
-            if mem[i]>-1:
-                return mem[i]
-            nextIndex=findNextJobIndex(startTime,jobs[i][1])
-            # do and nextindex or skip to next valid job in starttime
-            maxprofit=max(findMaxProfit(jobs,startTime,i+1),jobs[i][2]+findMaxProfit(jobs,startTime,nextIndex))
-            mem[i]=maxprofit
-            return maxprofit
-        return findMaxProfit(jobs,startTime,0)
+            _,end,p=jobs[i]
+            idx=bisect_left(jobs,end,lo=i+1,key=lambda x:x[0])
+            memo[i]=max(dfs(i+1),p+dfs(idx))
+            return memo[i]
+        return dfs(0)
