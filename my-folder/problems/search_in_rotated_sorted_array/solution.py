b6a2e8f4fa0d0dcb3127ad0c8ba6acb36369b3d5
OpenFusionUSA <web.developer@openfusion.us>
2024-09-12 22:06:19 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array (python3)

--- my-folder/problems/search_in_rotated_sorted_array/solution.py
+++ my-folder/problems/search_in_rotated_sorted_array/solution.py
@@ -1,19 +1,19 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        def binary_search(left,right):
+        def binarysearch(left,right):
             mid=(left+right)//2
             if nums[mid]==target:
                 return mid
             if left>right:
                 return -1
             if nums[mid]>=nums[left]:
-                if nums[mid]>target>=nums[left]:
-                    return binary_search(left,mid-1)
+                if nums[mid]>=target>=nums[left]:
+                    return binarysearch(left, mid-1)
                 else:
-                    return binary_search(mid+1,right)
+                    return binarysearch(mid+1, right)
             else:
-                if nums[mid]<target<=nums[right]:
-                    return binary_search(mid+1,right)
+                if nums[mid]<=target<=nums[right]:
+                    return binarysearch(mid+1, right)
                 else:
-                    return binary_search(left,mid-1)    
-        return binary_search(0,len(nums)-1)
+                    return binarysearch(left, mid-1)
+        return binarysearch(0, len(nums)-1)
