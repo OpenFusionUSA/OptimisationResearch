68385c38199f84b036d899f7a658fc5765603db1
OpenFusionUSA <web.developer@openfusion.us>
2024-09-12 22:48:52 +0000

    Sync LeetCode submission - Wildcard Matching (python3)

---
+++ my-folder/problems/wildcard_matching/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        @cache
+        def dfs(i,j):
+            if i>=len(s):
+                return j>=len(p) or (p[j]=="*" and dfs(i,j+1))
+            elif j>=len(p):
+                return False
+            if p[j]=="*":
+                return dfs(i+1, j) or dfs(i, j+1) or dfs(i+1, j+1)
+            return (p[j]=="?" or s[i]==p[j]) and dfs(i+1, j+1)
+        return dfs(0, 0)
