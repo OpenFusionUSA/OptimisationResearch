b07a03fd857060c86d4ce1d3eadf2a549ceecab0
OpenFusionUSA <web.developer@openfusion.us>
2024-09-12 15:10:52 +0000

    Sync LeetCode submission - Remove K Digits (python3)

--- my-folder/problems/remove_k_digits/solution.py
+++ my-folder/problems/remove_k_digits/solution.py
@@ -2,9 +2,9 @@ class Solution:
     def removeKdigits(self, num: str, k: int) -> str:
         stack=[]
         for digit in num:
-            while k and stack and stack[-1]>digit:
+            while k>0 and stack and stack[-1]>digit:
                 stack.pop()
                 k-=1
             stack.append(digit)
-        finalstack=stack[:-k] if k else stack
-        return "".join(finalstack).lstrip("0") or "0"
+        remaining=stack[:-k] if k else stack
+        return "".join(remaining).lstrip("0") or "0"
