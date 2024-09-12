1d082b1884fb358cea21d25eede67c80d5be8e90
OpenFusionUSA <web.developer@openfusion.us>
2024-09-12 16:58:49 +0000

    Sync LeetCode submission - House Robber II (python3)

--- my-folder/problems/house_robber_ii/solution.py
+++ my-folder/problems/house_robber_ii/solution.py
@@ -1,32 +1,22 @@
-class Solution(object):
-    def rob(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        def roblin(nums):
+            if len(nums)==0:
+                return
+            if len(nums)==1:
+                return nums[0]
+            if len(nums)==2:
+                return max(nums[0],nums[1])
+            amount=[0]*len(nums)
+            amount[0]=nums[0]
+            amount[1]=max(nums[0],nums[1])
+            for i in range(2,len(nums)):
+                amount[i]=max(amount[i-2]+nums[i],amount[i-1])
+            return amount[-1]
         if len(nums)==0:
-            return 
+            return
         if len(nums)==1:
             return nums[0]
         if len(nums)==2:
             return max(nums[0],nums[1])
-        return max(self.roblin(nums[0:len(nums)-1]),self.roblin(nums[1:]))
-
-    def roblin(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        print(nums)
-        if len(nums)==0:
-            return 
-        if len(nums)==1:
-            return nums[0]
-        if len(nums)==2:
-            return max(nums[0],nums[1])
-        amount=[0]*(len(nums))
-        amount[0]=nums[0]
-        amount[1]=max(amount[0],nums[1])
-        for i in range(2,len(nums)):
-            amount[i]=max(amount[i-1],amount[i-2]+nums[i])
-        return amount[-1]
+        return max(roblin(nums[:-1]),roblin(nums[1:]))
