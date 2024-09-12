413e42bbde64313517ba3c044c17d59d7cf9d44b
OpenFusionUSA <web.developer@openfusion.us>
2024-09-12 18:18:48 +0000

    Sync LeetCode submission - Koko Eating Bananas (python3)

--- my-folder/problems/koko_eating_bananas/solution.py
+++ my-folder/problems/koko_eating_bananas/solution.py
@@ -1,16 +1,16 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        piles.sort()
         n=len(piles)
-        l=1
-        r=max(piles)
-        midspeed=0
-        while l<=r:
-            midspeed=(l+r)//2
-            hoursspent=0
+        maxspeed=max(piles)
+        minspeed=1
+        while minspeed<=maxspeed:
+            midspeed=(minspeed+maxspeed)//2
+            timespent=0
             for p in piles:
-                hoursspent+=(p//midspeed)+ 1 if p%midspeed>0 else p//midspeed
-            if hoursspent>h:
-                l=midspeed+1
+                timespent+=(p//midspeed)+1 if p%midspeed>0 else p//midspeed
+            if timespent>h:
+                minspeed=midspeed+1
             else:
-                r=midspeed-1
-        return l
+                maxspeed=midspeed-1
+        return minspeed
