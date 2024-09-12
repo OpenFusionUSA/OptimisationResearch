d14efb25eb7151f2116de904722d5358e03b11a7
OpenFusionUSA <web.developer@openfusion.us>
2024-09-12 18:29:03 +0000

    Sync LeetCode submission - Jump Game II (python3)

--- my-folder/problems/jump_game_ii/solution.py
+++ my-folder/problems/jump_game_ii/solution.py
@@ -2,13 +2,12 @@ class Solution:
     def jump(self, nums: List[int]) -> int:
         n=len(nums)
         l,r=0,0
-        farthest=0
+        far=0
         ans=0
         while r<n-1:
             for i in range(l,r+1):
-                farthest=max(farthest,i+nums[i])
+                far=max(far,i+nums[i])
             l=r+1
-            r=farthest
+            r=far
             ans+=1
         return ans
-        
