46c7616b49c498beac4121d2a678a4ea3b8e810a
OpenFusionUSA <web.developer@openfusion.us>
2024-09-12 03:50:28 +0000

    Sync LeetCode submission - Smallest String With Swaps (python3)

--- my-folder/problems/smallest_string_with_swaps/solution.py
+++ my-folder/problems/smallest_string_with_swaps/solution.py
@@ -1,52 +1,15 @@
-
 from sortedcontainers import SortedList
-
-class UnionFind:
-    def __init__(self, size):
-        self.root = [i for i in range(size)]
-        self.rank = [1] * size
-
-    def find(self, x):
-        while x != self.root[x]:
-            x = self.root[x]
-        return x
-		
-    def union(self, x, y):
-        rootX = self.find(x)
-        rootY = self.find(y)
-        if rootX != rootY:
-            if self.rank[rootX] > self.rank[rootY]:
-                self.root[rootY] = rootX
-            elif self.rank[rootX] < self.rank[rootY]:
-                self.root[rootX] = rootY
-            else:
-                self.root[rootY] = rootX
-                self.rank[rootX] += 1
-
-    def connected(self, x, y):
-        return self.find(x) == self.find(y)
-    
-    def getMaxrank():
-        maxrank=max(self.rank)
-        
-    
-    
-class Solution(object):
-    def smallestStringWithSwaps(self, s, pairs):
-        """
-        :type s: str
-        :type pairs: List[List[int]]
-        :rtype: str
-        """
-        t=UnionFind(len(s))
-        for pair in pairs:
-            t.union(pair[0],pair[1])
-        lookup = defaultdict(SortedList)
-        for i, node in enumerate(t.root):
-            lookup[t.find(node)].add(s[i])
-        ans = []
-        for i in range(len(s)):
-            ans.append(lookup[t.find(i)].pop(0))
-        return ''.join(ans)
-        
-        
+class Solution:
+    def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:
+        n=len(s)
+        pos=[i for i in range(n)]
+        def find(x):
+            if pos[x]!=x:
+                pos[x]=find(pos[x])
+            return pos[x]
+        for x,y in pairs:
+            pos[find(x)]=find(y)
+        d=defaultdict(SortedList)
+        for i,c in enumerate(s):
+            d[find(i)].add(c)
+        return "".join(d[find(i)].pop(0) for i in range(n))
