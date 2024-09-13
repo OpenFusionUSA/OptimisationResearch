91d1b44685f62b22d45847cbe7783909e67fecde
OpenFusionUSA <web.developer@openfusion.us>
2024-09-13 21:30:16 +0000

    Sync LeetCode submission - Set Matrix Zeroes (python)

--- my-folder/problems/set_matrix_zeroes/solution.py
+++ my-folder/problems/set_matrix_zeroes/solution.py
@@ -2,26 +2,26 @@ class Solution(object):
     def setZeroes(self, matrix):
         """
         :type matrix: List[List[int]]
-        :rtype: void Do not return anything, modify matrix in-place instead.
+        :rtype: None Do not return anything, modify matrix in-place instead.
         """
-        is_col = False
-        R = len(matrix)
-        C = len(matrix[0])
-        for i in range(R):
-            if not is_col and matrix[i][0]==0:
-                is_col=True
-            for j in range(1,C):
+        if not matrix:
+            return matrix
+        m,n=len(matrix),len(matrix[0])
+        flag1=any(v==0 for v in matrix[0])
+        flag2=any(matrix[i][0]==0 for i in range(m))
+        for i in range(1,m):
+            for j in range(1,n):
                 if matrix[i][j]==0:
                     matrix[i][0]=matrix[0][j]=0
-        for i in range(1,R):
-            for j in range(1,C):
-                if not matrix[0][j] or not matrix[i][0]:
+        for i in range(1,m):
+            for j in range(1,n):
+                if matrix[i][j]!=0 and ( matrix[i][0]==0 or matrix[0][j]==0):
                     matrix[i][j]=0
-        if matrix[0][0]==0:
-                    for j in range(C):
-                        matrix[0][j]=0
-        if is_col:
-            for i in range(R):
-                matrix[i][0]=0
+        if flag1:
+            for i in range(n):
+                matrix[0][i]=0
         
-            
+        if flag2:
+            for i in range(m):
+                matrix[i][0]=0
+        return matrix
