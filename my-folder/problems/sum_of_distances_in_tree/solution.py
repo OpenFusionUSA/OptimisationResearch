e81d2f187c16a6c1230e2ad79be727ca1066aef0
OpenFusionUSA <web.developer@openfusion.us>
2024-09-13 03:50:08 +0000

    Sync LeetCode submission - Sum of Distances in Tree (python3)

--- my-folder/problems/sum_of_distances_in_tree/solution.py
+++ my-folder/problems/sum_of_distances_in_tree/solution.py
@@ -1,25 +1,23 @@
-class Solution(object):
-    def sumOfDistancesInTree(self, N, edges):
-        graph = collections.defaultdict(set)
-        for u, v in edges:
-            graph[u].add(v)
-            graph[v].add(u)
-
-        count = [1] * N
-        ans = [0] * N
-        def dfs(node = 0, parent = None):
-            for child in graph[node]:
-                if child != parent:
-                    dfs(child, node)
-                    count[node] += count[child]
-                    ans[node] += ans[child] + count[child]
-
-        def dfs2(node = 0, parent = None):
-            for child in graph[node]:
-                if child != parent:
-                    ans[child] = ans[node] - count[child] + N - count[child]
-                    dfs2(child, node)
-
-        dfs()
+class Solution:
+    def sumOfDistancesInTree(self, n: int, edges: List[List[int]]) -> List[int]:
+        count=[1]*n
+        ans=[0]*n
+        def dfs1(node=0,parent=None):
+            for adjnode in graph[node]:
+                if adjnode!=parent:
+                    dfs1(adjnode,node)
+                    count[node]+=count[adjnode]
+                    ans[node]+=ans[adjnode]+count[adjnode]
+        def dfs2(node=0,parent=None):
+            for adjnode in graph[node]:
+                if adjnode!=parent:
+                    ans[adjnode]=ans[node]-count[adjnode]+n-count[adjnode]
+                    dfs2(adjnode,node)
+        
+        graph=defaultdict(set)
+        for a,b in edges:
+            graph[a].add(b)
+            graph[b].add(a)
+        dfs1()
         dfs2()
         return ans
