99b6b64856497e9dfda3263a8c2a3d20ff19f9b1
OpenFusionUSA <web.developer@openfusion.us>
2024-09-13 00:02:41 +0000

    Sync LeetCode submission - Minimum Height Trees (python3)

--- my-folder/problems/minimum_height_trees/solution.py
+++ my-folder/problems/minimum_height_trees/solution.py
@@ -1,39 +1,23 @@
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
-
-        # edge cases
-        if n <= 2:
-            return [i for i in range(n)]
-
-        # Build the graph with the adjacency list
-        neighbors = [set() for i in range(n)]
-        for start, end in edges:
-            neighbors[start].add(end)
-            neighbors[end].add(start)
-
-        # Initialize the first layer of leaves
-        leaves = []
-        for i in range(n):
-            if len(neighbors[i]) == 1:
-                leaves.append(i)
-
-        # Trim the leaves until reaching the centroids
-        remaining_nodes = n
-        while remaining_nodes > 2:
-            remaining_nodes -= len(leaves)
-            new_leaves = []
-            # remove the current leaves along with the edges
-            while leaves:
-                leaf = leaves.pop()
-                # the only neighbor left for the leaf node
-                neighbor = neighbors[leaf].pop()
-                # remove the only edge left
-                neighbors[neighbor].remove(leaf)
-                if len(neighbors[neighbor]) == 1:
-                    new_leaves.append(neighbor)
-
-            # prepare for the next round
-            leaves = new_leaves
-
-        # The remaining nodes are the centroids of the graph
-        return leaves
+        if n==1:
+            return [0]
+        graph=[[] for _ in range(n)]
+        degree=[0]*n
+        for a,b in edges:
+            graph[a].append(b)
+            graph[b].append(a)
+            degree[a]+=1
+            degree[b]+=1
+        resp=[]
+        q=deque([i for i,v in enumerate(degree) if v==1])
+        while q:
+            resp.clear()
+            for _ in range(len(q)):
+                node=q.popleft()
+                resp.append(node)
+                for adjnode in graph[node]:
+                    degree[adjnode]-=1
+                    if degree[adjnode]==1:
+                        q.append(adjnode)
+        return resp
