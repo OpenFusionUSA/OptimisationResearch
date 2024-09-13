437f568ef05dec9944af569fea4f574780aacfee
OpenFusionUSA <web.developer@openfusion.us>
2024-09-13 00:18:34 +0000

    Sync LeetCode submission - Longest Repeating Character Replacement (python3)

--- my-folder/problems/longest_repeating_character_replacement/solution.py
+++ my-folder/problems/longest_repeating_character_replacement/solution.py
@@ -1,39 +1,11 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        def sumofotherchars(dic):
-            maxchar, maxcount = '',0
-            for k,v in dic.items():
-                if v>maxcount:
-                    maxchar=k
-                    maxcount=v
-            c = maxchar
-            total = 0
-            for k,v in dic.items():
-                if k!=c: total+=v
-            return total
-        dic = {}
-        start = 0
-        n=len(s)
-        maxlen = 0
-        for i in range(n):
-            if sumofotherchars(dic)==k:
-                # print(dic, start)
-                if s[i] not in dic:
-                    dic[s[i]] = 0
-                dic[s[i]] += 1
-                # print(dic,start, i)
-                # print(sumofotherchars(dic))
-                while sumofotherchars(dic) > k:
-                    # print(sumofotherchars(dic)
-                    # print(dic,start)
-                    dic[s[start]]-=1
-                    if not dic[s[start]]: del dic[s[start]]
-                    start += 1
-                    # print(dic,start)
-                maxlen = max(maxlen, i-start+1)
-            else:
-                # print(dic)
-                if s[i] not in dic: dic[s[i]] = 0
-                dic[s[i]]+=1
-                maxlen = max(maxlen, i-start+1)
-        return maxlen
+        count=Counter()
+        l,mx=0,0
+        for r,c in enumerate(s):
+            count[c]+=1
+            mx=max(mx, count[c])
+            if r-l+1-mx>k:
+                count[s[l]]-=1
+                l+=1
+        return len(s)-l
