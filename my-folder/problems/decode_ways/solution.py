a5446c74e7a4818d293f6b21dd1d846177b42cb8
OpenFusionUSA <web.developer@openfusion.us>
2024-09-13 02:32:07 +0000

    Sync LeetCode submission - Decode Ways (python3)

--- my-folder/problems/decode_ways/solution.py
+++ my-folder/problems/decode_ways/solution.py
@@ -1,24 +1,14 @@
 class Solution:
-
-    @lru_cache(maxsize=None)
-    def recursiveWithMemo(self, index, s) -> int:
-        # If you reach the end of the string
-        # Return 1 for success.
-        if index == len(s):
-            return 1
-
-        # If the string starts with a zero, it can't be decoded
-        if s[index] == '0':
-            return 0
-
-        if index == len(s)-1:
-            return 1
-        
-        answer = self.recursiveWithMemo(index + 1, s)
-        if int(s[index : index + 2]) <= 26:
-            answer += self.recursiveWithMemo(index + 2, s)
-
-        return answer
-
     def numDecodings(self, s: str) -> int:
-        return self.recursiveWithMemo(0, s)
+        dp={len(s):1}
+        def dfs(i):
+            if i in dp:
+                return dp[i]
+            if s[i]=="0":
+                return 0
+            resp=dfs(i+1)
+            if (i+1<len(s) and (s[i]=="1" or ( s[i]=="2" and s[i+1] in "0123456"))):
+                resp+=dfs(i+2)
+            dp[i]=resp
+            return resp
+        return dfs(0)
