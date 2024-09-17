616ccf66cbc3d2895eeef004be56289e133dcb9c
OpenFusionUSA <web.developer@openfusion.us>
2024-09-17 22:01:40 +0000

    Sync LeetCode submission - Uncommon Words from Two Sentences (python3)

---
+++ my-folder/problems/uncommon_words_from_two_sentences/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
+        count=defaultdict(int)
+        for word in s1.split():
+            count[word]+=1
+        for word in s2.split():
+            count[word]+=1
+        return [ word for word in count.keys() if count[word]==1]
